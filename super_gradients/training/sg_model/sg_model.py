model averaging fixed

super_gradients/training/sg_model/sg_model.py
@@ -447,7 +447,8 @@ class SgModel:
             logger.info("Best checkpoint overriden: validation " + self.metric_to_watch + ": " + str(metric))
 
         if self.training_params.average_best_models:
-            averaged_model_sd = self.model_weight_averaging.get_average_model(self.net,
+            net_for_averaging = self.ema_model.ema if self.ema else self.net
+            averaged_model_sd = self.model_weight_averaging.get_average_model(net_for_averaging,
                                                                               validation_results_tuple=validation_results_tuple)
             torch.save({'net': averaged_model_sd},
                        os.path.join(self.checkpoints_dir_path, self.average_model_checkpoint_filename))