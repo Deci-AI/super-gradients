90690d0d50a846303ab24fb021d53712b7caee50
shayaharon <shay.aharon@deci.ai>
2021-12-09 18:53:00 +0200

    batch acc fix in poly lr

--- super_gradients/training/utils/callbacks.py
+++ super_gradients/training/utils/callbacks.py
@@ -293,8 +293,8 @@ class PolyLRCallback(LRCallbackBase):
             effective_epoch = context.epoch - self.training_params.lr_warmup_epochs
             effective_max_epochs = self.max_epochs - self.training_params.lr_warmup_epochs
 
-            current_iter = self.train_loader_len * effective_epoch + context.batch_idx
-            max_iter = self.train_loader_len * effective_max_epochs
+            current_iter = (self.train_loader_len * effective_epoch + context.batch_idx)/self.training_params.batch_accumulate
+            max_iter = self.train_loader_len * effective_max_epochs/self.training_params.batch_accumulate
             self.lr = self.initial_lr * pow((1.0 - (current_iter / max_iter)), 0.9)
             self.update_lr(context.optimizer, context.epoch, context.batch_idx)
 
