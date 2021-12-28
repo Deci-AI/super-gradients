4de259bfc50832564cf9269ac59a8db463038c8f
shayaharon <shay.aharon@deci.ai>
2021-12-28 09:27:19 +0200

    urls added

--- super_gradients/training/pretrained_models.py
+++ super_gradients/training/pretrained_models.py
@@ -1,12 +1,21 @@
 MODEL_URLS = {"regnetY800_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/regnetY800_imagenet.pth",
+              "regnetY600_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/regnet600/average_model.pth",
+              "regnetY400_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/regnet400/average_model.pth",
+              "regnetY200_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/regnet200/average_model.pth",
+
               "resnet50_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/resnet50_imagenet.pth",
+              "resnet34_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/resnet34/average_model.pth",
+              "resnet18_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/resnet18/average_model.pth",
+
               "repvgg_a0_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/repvgg_a0_imagenet.pth",
-              "shelfnet34_coco_segmentation_subclass": "https://deci-pretrained-models.s3.amazonaws.com"
+
+              "shelfnet34_lw_coco_segmentation_subclass": "https://deci-pretrained-models.s3.amazonaws.com"
                                                        "/shelfnet34_coco_segmentation_subclass.pth",
-"ddrnet_23_cityscapes": "https://deci-pretrained-models.s3.amazonaws.com/ddrnet23_cityscapes"
-"/ddrnet23_cityscapes.pth ",
-"ddrnet_23_slim_cityscapes": "https://deci-pretrained-models.s3.amazonaws.com/ddrnet23_cityscapes/ddrnet23_slim_cityscapes.pth"
-}
+
+              "ddrnet_23_cityscapes": "https://deci-pretrained-models.s3.amazonaws.com/ddrnet23_cityscapes"
+                                      "/ddrnet23_cityscapes.pth ",
+              "ddrnet_23_slim_cityscapes": "https://deci-pretrained-models.s3.amazonaws.com/ddrnet23_cityscapes/ddrnet23_slim_cityscapes.pth"
+              }
 PRETRAINED_NUM_CLASSES = {"imagenet": 1000,
                           "coco_segmentation_subclass": 21,
-"cityscapes": 19}
+                          "cityscapes": 19}
