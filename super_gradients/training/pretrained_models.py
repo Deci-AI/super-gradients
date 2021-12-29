8139611d3d7642b74a7eeed98b4f4bd50deae95e
shayaharon <shay.aharon@deci.ai>
2021-12-29 15:32:50 +0200

    urls updated

--- super_gradients/training/pretrained_models.py
+++ super_gradients/training/pretrained_models.py
@@ -1,10 +1,10 @@
 MODEL_URLS = {"regnetY800_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/regnetY800_imagenet.pth",
-              "regnetY600_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/regnet600/average_model.pth",
-              "regnetY400_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/regnet400/average_model.pth",
-              "regnetY200_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/regnety200avg_model.pth",
+              "regnetY600_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/RegnetY600/average_model_regnety600.pth",
+              "regnetY400_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/RegnetY400/average_model_regnety400.pth",
+              "regnetY200_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/RegnetY200/average_model_regnety200.pth",
 
               "resnet50_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/resnet50_imagenet.pth",
-              "resnet34_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/resnet34/average_model.pth",
+              "resnet34_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/resent_34/average_model.pth",
               "resnet18_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/resnet18/average_model.pth",
 
               "repvgg_a0_imagenet": "https://deci-pretrained-models.s3.amazonaws.com/repvgg_a0_imagenet.pth",
