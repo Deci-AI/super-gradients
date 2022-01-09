2db3ccb9d845fbf8cf26c7dcc007513940fc65d2
shayaharon <shay.aharon@deci.ai>
2022-01-09 16:31:55 +0200

    docs added to example

--- src/super_gradients/examples/yolov5_transfer_learning_example/yolov5_transfer_learning_example.py
+++ src/super_gradients/examples/yolov5_transfer_learning_example/yolov5_transfer_learning_example.py
@@ -32,13 +32,14 @@ dataset_params = {"batch_size": 48,
                       "translate": 0.245,  # IMAGE TRANSLATION (+/- fraction)
                       "scale": 0.898,  # IMAGE SCALE (+/- gain)
                       "shear": 0.602,
-                      "mixup": 0.243
+                      "mixup": 0.243  # MIXUP PROBABILITY
                   }
                   }
 
 # INITIALIZE SG MODEL INSTANCE, AND A PASCAL VOC DATASET INTERFACE
 model = SgModel("yolov5m_pascal_finetune_augment_fix")
-dataset_interface = PascalVOCUnifiedDetectionDataSetInterface(dataset_params=dataset_params, cache_labels=True, cache_images=True)
+dataset_interface = PascalVOCUnifiedDetectionDataSetInterface(dataset_params=dataset_params, cache_labels=True,
+                                                              cache_images=True)
 
 # CONNECTING THE DATASET INTERFACE WILL SET SGMODEL'S CLASSES ATTRIBUTE ACCORDING TO PASCAL VOC
 model.connect_dataset_interface(dataset_interface, data_loader_num_workers=8)
@@ -47,7 +48,6 @@ model.connect_dataset_interface(dataset_interface, data_loader_num_workers=8)
 # LOADING THE PRETRAINED YOLO_V5M, IT WILL CALL IT'S REPLACE_HEAD METHOD AND CHANGE IT'S DETECT LAYER ACCORDING
 # TO PASCAL VOC CLASSES
 model.build_model("yolo_v5m", arch_params={"pretrained_weights": "coco"})
-
 
 # WE NOW TUNE THE 3 NORMALIZERS ACCORDING TO THE NEW DATASET ATTRIBUTES,
 network = model.net
@@ -59,30 +59,30 @@ num_branches_norm = 3. / num_levels
 num_classes_norm = len(model.classes) / 80.
 image_size_norm = train_image_size / 640.
 
-# DEFINE TRAINING PARAMS
+# DEFINE TRAINING PARAMS. SEE DOCS FOR THE FULL LIST.
 training_params = {"max_epochs": 50,
                    "lr_mode": "cosine",
                    "initial_lr": 0.0032,
                    "cosine_final_lr_ratio": 0.12,
                    "lr_warmup_epochs": 2,
-                   "batch_accumulate": 1,
-                   "warmup_bias_lr": 0.05,
+                   "warmup_bias_lr": 0.05,  # LR TO START FROM DURING WARMUP (DROPS DOWN DURING WARMUP EPOCHS) FOR BIAS.
                    "loss": "yolo_v5_loss",
                    "criterion_params": {"anchors": Anchors(
                        anchors_list=[[10, 13, 16, 30, 33, 23], [30, 61, 62, 45, 59, 119],
-                                     [116, 90, 156, 198, 373, 326]], strides=[8, 16, 32]),
-                       "box_loss_gain": 0.0296 * num_branches_norm,
-                       "cls_loss_gain": 0.243 * num_classes_norm * num_branches_norm,
-                       "cls_pos_weight": 0.631,
-                       "obj_loss_gain": 0.301 * image_size_norm ** 2 * num_branches_norm,
-                       "obj_pos_weight": 0.911,
-                       "anchor_threshold": 2.91},
+                                     [116, 90, 156, 198, 373, 326]], strides=[8, 16, 32]),  # MODEL'S ANCHORS
+                       "box_loss_gain": 0.0296 * num_branches_norm,  # COEF FOR BOX LOSS COMPONENT, NORMALIZED
+                       "cls_loss_gain": 0.243 * num_classes_norm * num_branches_norm,  # COEF FOR CLASSIFICATION
+                                                                                       # LOSS COMPONENT, NORMALIZED
+                       "cls_pos_weight": 0.631,  # CLASSIFICATION BCE POSITIVE CLASS WEIGHT
+                       "obj_loss_gain": 0.301 * image_size_norm ** 2 * num_branches_norm,  # OBJECT BCE COEF, NORMALIZED
+                       "obj_pos_weight": 0.911,  # OBJECT BCE POSITIVE CLASS WEIGHT
+                       "anchor_threshold": 2.91  # RATIO DEFINING THE SIZE RANGE OF AN ANCHOR.
+                   },
                    "optimizer": "SGD",
                    "warmup_momentum": 0.5,
                    "optimizer_params": {"momentum": 0.843,
                                         "weight_decay": 0.00036,
                                         "nesterov": True},
-                   "mixed_precision": False,
                    "ema": True,
                    "train_metrics_list": [],
                    "valid_metrics_list": [DetectionMetrics(post_prediction_callback=YoloV5PostPredictionCallback(),
