5300da3dfb9254e271e22fc990a249525bbdcb04
shayaharon <shay.aharon@deci.ai>
2022-01-09 15:58:32 +0200

    comments added

--- src/super_gradients/examples/yolov5_transfer_learning_example/yolov5_transfer_learning_example.py
+++ src/super_gradients/examples/yolov5_transfer_learning_example/yolov5_transfer_learning_example.py
@@ -15,6 +15,7 @@ super_gradients.init_trainer()
 
 distributed = super_gradients.is_distributed()
 
+# DEFINE DATASET PARAMS FOR PASCAL VOC
 dataset_params = {"batch_size": 48,
                   "val_batch_size": 48,
                   "train_image_size": 512,
@@ -35,17 +36,20 @@ dataset_params = {"batch_size": 48,
                   }
                   }
 
-
-model = SgModel("yolov5m_pascal_finetune_augment_fix",
-                multi_gpu=MultiGPUMode.OFF,
-                post_prediction_callback=YoloV5PostPredictionCallback())
-
+# INITIALIZE SG MODEL INSTANCE, AND A PASCAL VOC DATASET INTERFACE
+model = SgModel("yolov5m_pascal_finetune_augment_fix")
 dataset_interface = PascalVOCUnifiedDetectionDataSetInterface(dataset_params=dataset_params, cache_labels=True, cache_images=True)
+
+# CONNECTING THE DATASET INTERFACE WILL SET SGMODEL'S CLASSES ATTRIBUTE ACCORDING TO PASCAL VOC
 model.connect_dataset_interface(dataset_interface, data_loader_num_workers=8)
+
+# THIS IS WHERE THE MAGIC HAPPENS- SINCE SGMODEL'S CLASSES ATTRIBUTE WAS SET TO BE DIFFERENT FROM COCO'S, AFTER
+# LOADING THE PRETRAINED YOLO_V5M, IT WILL CALL IT'S REPLACE_HEAD METHOD AND CHANGE IT'S DETECT LAYER ACCORDING
+# TO PASCAL VOC CLASSES
 model.build_model("yolo_v5m", arch_params={"pretrained_weights": "coco"})
 
-post_prediction_callback = YoloV5PostPredictionCallback()
 
+# WE NOW TUNE THE 3 NORMALIZERS ACCORDING TO THE NEW DATASET ATTRIBUTES,
 network = model.net
 network = network.module if hasattr(network, 'module') else network
 num_levels = network._head._modules_list[-1].detection_layers_num
@@ -55,6 +59,7 @@ num_branches_norm = 3. / num_levels
 num_classes_norm = len(model.classes) / 80.
 image_size_norm = train_image_size / 640.
 
+# DEFINE TRAINING PARAMS
 training_params = {"max_epochs": 50,
                    "lr_mode": "cosine",
                    "initial_lr": 0.0032,
@@ -80,7 +85,7 @@ training_params = {"max_epochs": 50,
                    "mixed_precision": False,
                    "ema": True,
                    "train_metrics_list": [],
-                   "valid_metrics_list": [DetectionMetrics(post_prediction_callback=post_prediction_callback,
+                   "valid_metrics_list": [DetectionMetrics(post_prediction_callback=YoloV5PostPredictionCallback(),
                                                            num_cls=len(
                                                                dataset_interface.classes))],
                    "loss_logging_items_names": ["GIoU", "obj", "cls", "Loss"],
@@ -88,4 +93,5 @@ training_params = {"max_epochs": 50,
                    "greater_metric_to_watch_is_better": True,
                    "warmup_mode": "yolov5_warmup"}
 
+# FINALLY, CALL TRAIN
 model.train(training_params=training_params)
