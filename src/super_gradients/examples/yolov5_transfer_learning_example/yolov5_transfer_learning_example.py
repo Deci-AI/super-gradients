a38986f966a33efd60b61e439f3643bf22e1d369
shayaharon <shay.aharon@deci.ai>
2022-01-18 16:09:48 +0200

    experimental files removed

--- src/super_gradients/examples/yolov5_transfer_learning_example/yolov5_transfer_learning_example.py
+++ src/super_gradients/examples/yolov5_transfer_learning_example/yolov5_transfer_learning_example.py
@@ -10,7 +10,6 @@ from super_gradients.training.models.detection_models.yolov5 import YoloV5PostPr
 from super_gradients.training.utils.detection_utils import base_detection_collate_fn
 from super_gradients.training.metrics import DetectionMetrics
 from super_gradients.training.utils.detection_utils import Anchors
-from super_gradients.training.utils.detection_utils import AnchorGenerator
 
 super_gradients.init_trainer()
 
@@ -25,56 +24,30 @@ dataset_params = {"batch_size": 48,
                   "train_collate_fn": base_detection_collate_fn,
                   "train_sample_loading_method": "mosaic",
                   "val_sample_loading_method": "default",
-                  # "dataset_hyper_param": {
-                  #     "hsv_h": 0.0138,  # IMAGE HSV-Hue AUGMENTATION (fraction)
-                  #     "hsv_s": 0.664,  # IMAGE HSV-Saturation AUGMENTATION (fraction)
-                  #     "hsv_v": 0.464,  # IMAGE HSV-Value AUGMENTATION (fraction)
-                  #     "degrees": 0.373,  # IMAGE ROTATION (+/- deg)
-                  #     "translate": 0.245,  # IMAGE TRANSLATION (+/- fraction)
-                  #     "scale": 0.898,  # IMAGE SCALE (+/- gain)
-                  #     "shear": 0.602,
-                  #     #"mixup": 0.243  # MIXUP PROBABILITY
-                  #},
                   "dataset_hyper_param": {
-                      "hsv_h": 0.015,  # IMAGE HSV-Hue AUGMENTATION (fraction)
-                      "hsv_s": 0.7,  # IMAGE HSV-Saturation AUGMENTATION (fraction)
-                      "hsv_v": 0.4,  # IMAGE HSV-Value AUGMENTATION (fraction)
-                      "degrees": 0.0,  # IMAGE ROTATION (+/- deg)
-                      "translate": 0.1,  # IMAGE TRANSLATION (+/- fraction)
-                      "scale": 0.5,  # IMAGE SCALE (+/- gain)
-                      "shear": 0.0}  # IMAGE SHEAR (+/- deg)
-                  ,
-                  "class_inclusion_list": ['person']
-
+                      "hsv_h": 0.0138,  # IMAGE HSV-Hue AUGMENTATION (fraction)
+                      "hsv_s": 0.664,  # IMAGE HSV-Saturation AUGMENTATION (fraction)
+                      "hsv_v": 0.464,  # IMAGE HSV-Value AUGMENTATION (fraction)
+                      "degrees": 0.373,  # IMAGE ROTATION (+/- deg)
+                      "translate": 0.245,  # IMAGE TRANSLATION (+/- fraction)
+                      "scale": 0.898,  # IMAGE SCALE (+/- gain)
+                      "shear": 0.602,
+                      "mixup": 0.243  # MIXUP PROBABILITY
                   }
-#
-dataset_params_rect = {"batch_size": 1,
-                       "val_batch_size": 1,
-                       "train_image_size": 512,
-                       "val_image_size": 512,
-                       "train_sample_loading_method": "default",
-                       "class_inclusion_list": ['person']
-                       }
-dataset_interface = PascalVOCUnifiedDetectionDataSetInterface(dataset_params=dataset_params_rect, cache_labels=True,
-                                                              cache_images=True)
+                  }
 
-anch_gen = AnchorGenerator()
-anchors = anch_gen(dataset=dataset_interface.trainset, thresh=1 / 2.91)
-anchors = Anchors(anchors_list=anchors,
-                  strides=[8, 16, 32])
 # INITIALIZE SG MODEL INSTANCE, AND A PASCAL VOC DATASET INTERFACE
-model = SgModel("yolov5nano_autoanch_wdefaultsm_pascal_person_finetune_lightaug_default_wdm")
+model = SgModel("yolov5m_pascal_finetune_augment_fix")
 dataset_interface = PascalVOCUnifiedDetectionDataSetInterface(dataset_params=dataset_params, cache_labels=True,
                                                               cache_images=True)
 
-print("num_cls" + str(len(dataset_interface.classes)))
 # CONNECTING THE DATASET INTERFACE WILL SET SGMODEL'S CLASSES ATTRIBUTE ACCORDING TO PASCAL VOC
 model.connect_dataset_interface(dataset_interface, data_loader_num_workers=8)
 
 # THIS IS WHERE THE MAGIC HAPPENS- SINCE SGMODEL'S CLASSES ATTRIBUTE WAS SET TO BE DIFFERENT FROM COCO'S, AFTER
 # LOADING THE PRETRAINED YOLO_V5M, IT WILL CALL IT'S REPLACE_HEAD METHOD AND CHANGE IT'S DETECT LAYER ACCORDING
 # TO PASCAL VOC CLASSES
-model.build_model("yolo_v5n", arch_params={"pretrained_weights": "coco", "anchors": anchors})
+model.build_model("yolo_v5m", arch_params={"pretrained_weights": "coco"})
 
 # WE NOW TUNE THE 3 NORMALIZERS ACCORDING TO THE NEW DATASET ATTRIBUTES,
 network = model.net
@@ -94,25 +67,21 @@ training_params = {"max_epochs": 50,
                    "lr_warmup_epochs": 2,
                    "warmup_bias_lr": 0.05,  # LR TO START FROM DURING WARMUP (DROPS DOWN DURING WARMUP EPOCHS) FOR BIAS.
                    "loss": "yolo_v5_loss",
-                   "criterion_params": {"anchors": anchors,#Anchors(anchors_list=anchors,
-                                                           # anchors_list=[[10, 13, 16, 30, 33, 23], [30, 61, 62, 45, 59, 119],
-                                                           #               [116, 90, 156, 198, 373, 326]],
-                                                           #strides=[8, 16, 32]),  # MODEL'S ANCHORS
-                                        "box_loss_gain": 0.0296 * num_branches_norm,
-                                        # COEF FOR BOX LOSS COMPONENT, NORMALIZED
-                                        "cls_loss_gain": 0.243 * num_classes_norm * num_branches_norm,
-                                        # COEF FOR CLASSIFICATION
-                                        # LOSS COMPONENT, NORMALIZED
-                                        "cls_pos_weight": 0.631,  # CLASSIFICATION BCE POSITIVE CLASS WEIGHT
-                                        "obj_loss_gain": 0.301 * image_size_norm ** 2 * num_branches_norm,
-                                        # OBJECT BCE COEF, NORMALIZED
-                                        "obj_pos_weight": 0.911,  # OBJECT BCE POSITIVE CLASS WEIGHT
-                                        "anchor_threshold": 2.91  # RATIO DEFINING THE SIZE RANGE OF AN ANCHOR.
-                                        },
+                   "criterion_params": {"anchors": Anchors(
+                       anchors_list=[[10, 13, 16, 30, 33, 23], [30, 61, 62, 45, 59, 119],
+                                     [116, 90, 156, 198, 373, 326]], strides=[8, 16, 32]),  # MODEL'S ANCHORS
+                       "box_loss_gain": 0.0296 * num_branches_norm,  # COEF FOR BOX LOSS COMPONENT, NORMALIZED
+                       "cls_loss_gain": 0.243 * num_classes_norm * num_branches_norm,  # COEF FOR CLASSIFICATION
+                                                                                       # LOSS COMPONENT, NORMALIZED
+                       "cls_pos_weight": 0.631,  # CLASSIFICATION BCE POSITIVE CLASS WEIGHT
+                       "obj_loss_gain": 0.301 * image_size_norm ** 2 * num_branches_norm,  # OBJECT BCE COEF, NORMALIZED
+                       "obj_pos_weight": 0.911,  # OBJECT BCE POSITIVE CLASS WEIGHT
+                       "anchor_threshold": 2.91  # RATIO DEFINING THE SIZE RANGE OF AN ANCHOR.
+                   },
                    "optimizer": "SGD",
                    "warmup_momentum": 0.5,
-                   "optimizer_params": {"momentum": 0.9,#0.843,
-                                        "weight_decay": 0.0001,#0.00036,
+                   "optimizer_params": {"momentum": 0.843,
+                                        "weight_decay": 0.00036,
                                         "nesterov": True},
                    "ema": True,
                    "train_metrics_list": [],
