d762aa61b5b2e304baf136715d60b4c679b0150d
shairoz <shai.rozenberg@deci.ai>
2022-03-14 14:30:36 +0200

    added grad_clip to default param yaml + changed grad_clip assertion

--- src/super_gradients/training/sg_model/sg_model.py
+++ src/super_gradients/training/sg_model/sg_model.py
@@ -863,7 +863,7 @@ class SgModel:
 
         # VERIFY GRADIENT CLIPPING VALUE
         if self.training_params.clip_grad_norm is not None and self.training_params.clip_grad_norm <= 0:
-            raise Exception('Params', 'Invalid clip_grad_norm')
+            raise TypeError('Params', 'Invalid clip_grad_norm')
 
         if self.load_checkpoint and load_opt_params:
             self.optimizer.load_state_dict(self.checkpoint['optimizer_state_dict'])
