7e73a62ad21d66d5c7bdb0274bf97dc17b179f44
shayaharon <shay.aharon@deci.ai>
2022-03-14 13:39:10 +0200

    linter

--- src/super_gradients/__init__.py
+++ src/super_gradients/__init__.py
@@ -6,4 +6,3 @@ from super_gradients.examples.train_from_recipe_example import train_from_recipe
 __all__ = ['ARCHITECTURES', 'losses', 'utils', 'datasets_utils', 'DataAugmentation',
            'TestDatasetInterface', 'SgModel', 'SegmentationTestDatasetInterface', 'DetectionTestDatasetInterface',
            'ClassificationTestDatasetInterface', 'init_trainer', 'is_distributed', 'train_from_recipe']
-
