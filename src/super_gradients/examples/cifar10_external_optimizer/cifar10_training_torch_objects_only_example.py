60207aea0796c3cd01969bcee0010fcae4d8f095
najeeb5 <najeeb.nabwani@deci.ai>
2022-04-25 14:01:38 +0300

    Modified example to showcase SG's flexibility and how to use torch objects when training with SG

--- src/super_gradients/examples/cifar10_external_optimizer/cifar10_training_torch_objects_only_example.py
+++ src/super_gradients/examples/cifar10_external_optimizer/cifar10_training_torch_objects_only_example.py
@@ -1,50 +1,62 @@
 
 """
-Main purpose is to demonstrate the use of initialized optimizers and lr_Schedulers in training.
+Cifar10 training with SuperGradients training with the following initialized torch objects:
+
+    DataLoaders
+    Optimizers
+    Networks (nn.Module)
+    Schedulers
+    Loss functions
+
+Main purpose is to demonstrate training in SG with minimal abstraction and maximal flexibility
 """
-import super_gradients
 
 from super_gradients import SgModel
-from super_gradients.training.datasets.dataset_interfaces.dataset_interface import Cifar10DatasetInterface
 from super_gradients.training.metrics.classification_metrics import Accuracy, Top5
 from super_gradients.training import MultiGPUMode
 from torch.optim import ASGD
 from torch.optim.lr_scheduler import MultiStepLR, ReduceLROnPlateau
-from super_gradients.training.models import ResNet18Cifar
+from torch.nn import CrossEntropyLoss
 from super_gradients.training.utils.callbacks import Phase, LRSchedulerCallback
-from super_gradients.training import utils
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import ToTensor
+from torchvision.models import resnet18
+from torch.utils.data import DataLoader
 
-super_gradients.init_trainer()
+
+# Define any torch DataLoaders, need at least train & valid loaders
+train_dataset = CIFAR10(root='data/', train=True, transform=ToTensor())
+valid_dataset = CIFAR10(root='data/', train=False, transform=ToTensor())
+
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=16)
+valid_loader = DataLoader(valid_dataset, batch_size=32)
+
+# Define any network of type nn.Module
+net = resnet18(num_classes=len(train_dataset.classes))
+
+# Define any optimizer of type torch.optim.Optimizer (and schedulers)
 lr = 2.5e-4
-net = ResNet18Cifar(arch_params=utils.HpmStruct(**{"num_classes": 10}))
-
-# Define optimizer and schedulers
 optimizer = ASGD(net.parameters(), lr=lr, weight_decay=0.0001)
 
 rop_lr_scheduler = ReduceLROnPlateau(optimizer, mode="max", patience=10, verbose=True)
 step_lr_scheduler = MultiStepLR(optimizer, milestones=[0, 150, 200], gamma=0.1)
 
-# Learning rate will be decreased after validation accuracy did not increase for 10 epochs, or at the specified
-# milestones. Notice how the callback for reduce on plateau scheduler is been called on Phase.VALIDATION_EPOCH_END
-# which causes it to take the accuracy value from the validation metrics.
+# Define any loss function of type torch.nn.modules.loss._Loss
+loss_fn = CrossEntropyLoss()
 
+# Define phase callbacks
 phase_callbacks = [LRSchedulerCallback(scheduler=rop_lr_scheduler, phase=Phase.VALIDATION_EPOCH_END, metric_name="Accuracy"),
                    LRSchedulerCallback(scheduler=step_lr_scheduler, phase=Phase.TRAIN_EPOCH_END)]
 
-# Define Model
-model = SgModel("Cifar10_external_objects_example", multi_gpu=MultiGPUMode.OFF)
-
-# Connect Dataset
-dataset = Cifar10DatasetInterface(dataset_params={"batch_size": 64})
-model.connect_dataset_interface(dataset, data_loader_num_workers=8)
-
-# Build Model
+# Bring everything together with SgModel and start training
+model = SgModel("Cifar10_external_objects_example", multi_gpu=MultiGPUMode.OFF,
+                train_loader=train_loader, valid_loader=valid_loader, classes=train_dataset.classes)
 model.build_model(net)
 
 train_params = {"max_epochs": 300,
                 "phase_callbacks": phase_callbacks,
                 "initial_lr": lr,
-                "loss": "cross_entropy",
+                "loss": loss_fn,
                 "criterion_params": {},
                 'optimizer': optimizer,
                 "train_metrics_list": [Accuracy(), Top5()],
