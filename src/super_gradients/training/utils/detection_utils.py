dfafd36ff6d16f7dd12a0c9426305515f77dc4d2
shayaharon <shay.aharon@deci.ai>
2022-04-28 14:05:57 +0300

    ul methods removed

--- src/super_gradients/training/utils/detection_utils.py
+++ src/super_gradients/training/utils/detection_utils.py
@@ -264,33 +264,6 @@ class IouThreshold(tuple, Enum):
         return self[0] != self[1]
 
 
-def box_iou(box1, box2):
-    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
-    """
-    Return intersection-over-union (Jaccard index) of boxes.
-    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
-    Arguments:
-        box1 (Tensor[N, 4])
-        box2 (Tensor[M, 4])
-    Returns:
-        iou (Tensor[N, M]): the NxM matrix containing the pairwise
-            IoU values for every element in boxes1 and boxes2
-    Taken from Yolov5 Ultralitics repo
-    """
-
-    def box_area(box):
-        # box = 4xn
-        return (box[2] - box[0]) * (box[3] - box[1])
-
-    area1 = box_area(box1.T)
-    area2 = box_area(box2.T)
-
-    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
-    inter = (torch.min(box1[:, None, 2:], box2[:, 2:]) - torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
-    return inter / (area1[:, None] + area2 - inter)  # iou = inter / (area1 + area2 - inter)
-
-
-
 def check_img_size_divisibilty(img_size: int, stride: int = 32):
     """
     :param img_size: Int, the size of the image (H or W).
