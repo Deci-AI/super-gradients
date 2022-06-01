f1eecc6a0c76f8d806367d0fa8669316d3d88e3a
shayaharon <shay.aharon@deci.ai>
2022-06-01 11:25:43 +0300

    yolox related code added

--- src/super_gradients/training/models/detection_models/yolox.py
+++ src/super_gradients/training/models/detection_models/yolox.py
@@ -1,43 +1,50 @@
 from super_gradients.training.models.detection_models.yolov5_base import YoLoV5Base, YoLoV5DarknetBackbone
-from super_gradients.training.utils.utils import HpmStruct, get_param
+from super_gradients.training.utils.utils import HpmStruct
 
 
-class Custom_YoLoV5(YoLoV5Base):
-    def __init__(self, arch_params: HpmStruct):
-        backbone = get_param(arch_params, 'backbone', YoLoV5DarknetBackbone)
-        super().__init__(backbone=backbone, arch_params=arch_params)
-
-
-class YoLoV5N(YoLoV5Base):
+class YoloX_N(YoLoV5Base):
     def __init__(self, arch_params: HpmStruct):
         arch_params.depth_mult_factor = 0.33
         arch_params.width_mult_factor = 0.25
+        arch_params.yolo_type = 'yoloX'
         super().__init__(backbone=YoLoV5DarknetBackbone, arch_params=arch_params)
 
 
-class YoLoV5S(YoLoV5Base):
+class YoloX_T(YoLoV5Base):
+    def __init__(self, arch_params: HpmStruct):
+        arch_params.depth_mult_factor = 0.33
+        arch_params.width_mult_factor = 0.375
+        arch_params.yolo_type = 'yoloX'
+        super().__init__(backbone=YoLoV5DarknetBackbone, arch_params=arch_params)
+
+
+class YoloX_S(YoLoV5Base):
     def __init__(self, arch_params: HpmStruct):
         arch_params.depth_mult_factor = 0.33
         arch_params.width_mult_factor = 0.50
+        arch_params.yolo_type = 'yoloX'
         super().__init__(backbone=YoLoV5DarknetBackbone, arch_params=arch_params)
 
 
-class YoLoV5M(YoLoV5Base):
+class YoloX_M(YoLoV5Base):
     def __init__(self, arch_params: HpmStruct):
         arch_params.depth_mult_factor = 0.67
         arch_params.width_mult_factor = 0.75
+        arch_params.yolo_type = 'yoloX'
         super().__init__(backbone=YoLoV5DarknetBackbone, arch_params=arch_params)
 
 
-class YoLoV5L(YoLoV5Base):
+class YoloX_L(YoLoV5Base):
     def __init__(self, arch_params: HpmStruct):
         arch_params.depth_mult_factor = 1.0
         arch_params.width_mult_factor = 1.0
+        arch_params.yolo_type = 'yoloX'
         super().__init__(backbone=YoLoV5DarknetBackbone, arch_params=arch_params)
 
 
-class YoLoV5X(YoLoV5Base):
+class YoloX_X(YoLoV5Base):
     def __init__(self, arch_params: HpmStruct):
         arch_params.depth_mult_factor = 1.33
         arch_params.width_mult_factor = 1.25
+        arch_params.yolo_type = 'yoloX'
         super().__init__(backbone=YoLoV5DarknetBackbone, arch_params=arch_params)
