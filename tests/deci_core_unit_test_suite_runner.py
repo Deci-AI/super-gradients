5155e2a5519586ef80a04e8dedcfa9e7033eb5ae
liork <lior.kadoch@deci.ai>
2022-07-10 10:49:30 +0300

    Add mask loss unit test

--- tests/deci_core_unit_test_suite_runner.py
+++ tests/deci_core_unit_test_suite_runner.py
@@ -20,6 +20,7 @@ from tests.unit_tests.yolox_unit_test import TestYOLOX
 from tests.unit_tests.lr_cooldown_test import LRCooldownTest
 from tests.unit_tests.detection_targets_format_transform_test import DetectionTargetsTransformTest
 from tests.unit_tests.forward_pass_prep_fn_test import ForwardpassPrepFNTest
+from tests.unit_tests.mask_loss_test import MaskAttentionLossTest
 
 
 class CoreUnitTestSuiteRunner:
@@ -63,6 +64,7 @@ class CoreUnitTestSuiteRunner:
         self.unit_tests_suite.addTest(self.test_loader.loadTestsFromModule(LRCooldownTest))
         self.unit_tests_suite.addTest(self.test_loader.loadTestsFromModule(DetectionTargetsTransformTest))
         self.unit_tests_suite.addTest(self.test_loader.loadTestsFromModule(ForwardpassPrepFNTest))
+        self.unit_tests_suite.addTest(self.test_loader.loadTestsFromModule(MaskAttentionLossTest))
 
     def _add_modules_to_end_to_end_tests_suite(self):
         """
