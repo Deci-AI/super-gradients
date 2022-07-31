4670161caf609a28594a8c984fc625eed19270c9
Louis Dupont <louis-dupont@live.fr>
2022-07-31 16:16:53 +0300

    add ref to net detectiondatasetv2 in inits

--- src/super_gradients/training/datasets/__init__.py
+++ src/super_gradients/training/datasets/__init__.py
@@ -5,6 +5,7 @@ from super_gradients.training.datasets.sg_dataset import ListDataset, DirectoryD
 from super_gradients.training.datasets.all_datasets import CLASSIFICATION_DATASETS, OBJECT_DETECTION_DATASETS, \
     SEMANTIC_SEGMENTATION_DATASETS
 from super_gradients.training.datasets.detection_datasets.detection_dataset import DetectionDataSet
+from super_gradients.training.datasets.detection_datasets.detection_dataset_v2 import DetectionDataSetV2
 from super_gradients.training.datasets.detection_datasets.coco_detection import COCODetectionDataSet
 from super_gradients.training.datasets.segmentation_datasets.segmentation_dataset import SegmentationDataSet
 from super_gradients.training.datasets.segmentation_datasets.pascal_voc_segmentation import PascalVOC2012SegmentationDataSet
@@ -26,4 +27,4 @@ __all__ = ['DataAugmentation', 'ListDataset', 'DirectoryDataSet', 'CLASSIFICATIO
            'PascalVOC2012SegmentationDataSetInterface', 'PascalAUG2012SegmentationDataSetInterface',
            'TestYoloDetectionDatasetInterface', 'DetectionTestDatasetInterface', 'ClassificationTestDatasetInterface',
            'SegmentationTestDatasetInterface',
-           'ImageNetDatasetInterface']
+           'ImageNetDatasetInterface', 'DetectionDataSetV2']
--- src/super_gradients/training/datasets/detection_datasets/__init__.py
+++ src/super_gradients/training/datasets/detection_datasets/__init__.py
@@ -3,6 +3,9 @@ from super_gradients.training.datasets.detection_datasets.coco_detection import 
 from super_gradients.training.datasets.detection_datasets.coco_detection_yolox import COCODetectionDatasetV2
 from super_gradients.training.datasets.detection_datasets.pascal_voc_detection import PascalVOCDetectionDataSet
 from super_gradients.training.datasets.detection_datasets.pascal_voc_detection_v2 import PascalVOCDetectionDataSetV2
+from super_gradients.training.datasets.detection_datasets.detection_dataset_v2 import DetectionDataSetV2
 
-__all__ = ['DetectionDataSet', 'COCODetectionDataSet', 'COCODetectionDatasetV2',
+
+__all__ = ['DetectionDataSet', 'COCODetectionDataSet', 'DetectionDataSetV2', 'COCODetectionDatasetV2',
            'PascalVOCDetectionDataSet', 'PascalVOCDetectionDataSetV2']
+DetectionDataSetV2 = 1
