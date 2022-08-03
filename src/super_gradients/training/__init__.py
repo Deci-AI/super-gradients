41422a17dc1dd453f0d6460b547c2cb4b361efd8
shayaharon <shay.aharon@deci.ai>
2022-08-03 12:16:35 +0300

    broken import fix

--- src/super_gradients/training/__init__.py
+++ src/super_gradients/training/__init__.py
@@ -1,11 +1,11 @@
 # PACKAGE IMPORTS FOR EXTERNAL USAGE
 import super_gradients.training.utils.distributed_training_utils as distributed_training_utils
-from super_gradients.training.datasets import datasets_utils, DataAugmentation, DetectionDataSet, TestDatasetInterface, SegmentationTestDatasetInterface,\
+from super_gradients.training.datasets import datasets_utils, DataAugmentation, TestDatasetInterface, SegmentationTestDatasetInterface,\
     DetectionTestDatasetInterface, ClassificationTestDatasetInterface
 from super_gradients.training.models import ARCHITECTURES
 from super_gradients.training.sg_model import SgModel, MultiGPUMode, StrictLoad
 from super_gradients.training.kd_model import KDModel
 
-__all__ = ['distributed_training_utils', 'datasets_utils', 'DataAugmentation', 'DetectionDataSet', 'TestDatasetInterface', 'ARCHITECTURES', 'SgModel',
+__all__ = ['distributed_training_utils', 'datasets_utils', 'DataAugmentation', 'TestDatasetInterface', 'ARCHITECTURES', 'SgModel',
            'KDModel', 'MultiGPUMode', 'TestDatasetInterface', 'SegmentationTestDatasetInterface', 'DetectionTestDatasetInterface',
            'ClassificationTestDatasetInterface', 'StrictLoad']
