5076909e9c704a7c5175e6282ebb3f50b63214be
Louis Dupont <louis-dupont@live.fr>
2022-08-13 23:28:51 +0300

    wip

--- src/super_gradients/examples/notebooks_params/quickstart_detection.py
+++ src/super_gradients/examples/notebooks_params/quickstart_detection.py
@@ -1,5 +1,24 @@
 from super_gradients.training.datasets.datasets_utils import DetectionMultiscalePrePredictionCallback
 
+DEFAULT_TRANSFORMS = {
+  "mixup_prob": 1.0,  # probability to apply per-sample mixup
+  "degrees": 10.,  # rotation degrees, randomly sampled from [-degrees, degrees]
+  "shear": 2.0,  # shear degrees, randomly sampled from [-degrees, degrees]
+  "flip_prob": 0.5,  # probability to apply horizontal flip
+  "hsv_prob": 1.0,  # probability to apply HSV transform
+  "hgain": 5,  # HSV transform hue gain (randomly sampled from [-hgain, hgain])
+  "sgain": 30,  # HSV transform saturation gain (randomly sampled from [-sgain, sgain])
+  "vgain": 30,  # HSV transform value gain (randomly sampled from [-vgain, vgain])
+  "mosaic_scale": [0.1, 2],  # random rescale range (keeps size by padding/cropping) after mosaic transform.
+  "mixup_scale": [0.5, 1.5],  # random rescale range for the additional sample in mixup
+  "mosaic_prob": 1.,  # probability to apply mosaic
+  "translate": 0.1,  # image translation fraction
+  "filter_box_candidates": False,  # whether to filter out transformed bboxes by edge size, area ratio, and aspect ratio.
+  "wh_thr": 2,  # edge size threshold when filter_box_candidates = True (pixels)
+  "ar_thr": 20,  # aspect ratio threshold when filter_box_candidates = True
+  "area_thr": 0.1,  # threshold for area ratio between original image and the transformed one, when when filter_box_candidates = True
+}
+
 DEFAULT_TRAINING_PARAMS = {
   "max_epochs": 20,
   "lr_mode": "cosine",
