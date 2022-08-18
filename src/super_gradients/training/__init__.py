706184b6fd5663bd946268c58ecda270d0d7f5e9
ofri masad <ofri@deci.ai>
2022-08-18 14:34:24 +0300

    fix train_from_recipe

--- src/super_gradients/training/__init__.py
+++ src/super_gradients/training/__init__.py
@@ -3,9 +3,13 @@ import super_gradients.training.utils.distributed_training_utils as distributed_
 from super_gradients.training.datasets import datasets_utils, DataAugmentation, TestDatasetInterface, SegmentationTestDatasetInterface, \
     DetectionTestDatasetInterface, ClassificationTestDatasetInterface
 from super_gradients.training.models import ARCHITECTURES
-from super_gradients.training.sg_trainer import Trainer, \
-    MultiGPUMode, StrictLoad
+from super_gradients.training.sg_trainer import Trainer
 from super_gradients.training.kd_trainer import KDTrainer
+from sg_model import SGModel
+from super_gradients.common import MultiGPUMode, StrictLoad, EvaluationType
+
+__all__ = ['SGModel', 'MultiGPUMode', 'StrictLoad', 'EvaluationType']
+
 
 __all__ = ['distributed_training_utils', 'datasets_utils', 'DataAugmentation', 'TestDatasetInterface',
            'ARCHITECTURES', 'Trainer', 'KDTrainer', 'MultiGPUMode', 'TestDatasetInterface', 'SegmentationTestDatasetInterface', 'DetectionTestDatasetInterface',
---
+++ src/super_gradients/training/sg_model/__init__.py
@@ -0,0 +1,5 @@
+# PACKAGE IMPORTS FOR EXTERNAL USAGE
+from super_gradients.common import MultiGPUMode, StrictLoad, EvaluationType
+from super_gradients.training.sg_model.sg_model import SGModel
+
+__all__ = ['SGModel', 'MultiGPUMode', 'StrictLoad', 'EvaluationType']
