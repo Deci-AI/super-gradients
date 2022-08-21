b8e8176421c35c455b2d5cd27d2c91f4007b8d26
liork <lior.kadoch@deci.ai>
2022-08-21 22:18:03 +0300

    add to architectures

--- src/super_gradients/training/models/all_architectures.py
+++ src/super_gradients/training/models/all_architectures.py
@@ -21,6 +21,7 @@ from super_gradients.training.models.segmentation_models.stdc import STDC1Classi
 
 from super_gradients.training.models.kd_modules.kd_module import KDModule
 from super_gradients.training.models.classification_models.beit import beit_base_patch16_224, beit_large_patch16_224
+from super_gradients.training.models.segmentation_models.ppliteseg import PPLiteSegT, PPLiteSegB
 # IF YOU UPDATE THE ARCHITECTURE DICT PLEASE ALSO UPDATE THE ENUM CLASS DOWN BELOW.
 
 
@@ -112,7 +113,9 @@ ARCHITECTURES = {"resnet18": resnet.ResNet18,
                  "vit_large": vit_large,
                  "vit_huge": vit_huge,
                  "beit_base_patch16_224": beit_base_patch16_224,
-                 "beit_large_patch16_224": beit_large_patch16_224
+                 "beit_large_patch16_224": beit_large_patch16_224,
+                 "pp_liteseg_t": PPLiteSegT,
+                 "pp_liteseg_b": PPLiteSegB,
                  }
 
 KD_ARCHITECTURES = {
