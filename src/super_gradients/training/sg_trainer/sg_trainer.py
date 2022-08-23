237499f454c0829ef961a63d36ad4c0bc89a5a1e
shayaharon <shay.aharon@deci.ai>
2022-08-23 15:02:08 +0300

    lint

--- src/super_gradients/training/sg_trainer/sg_trainer.py
+++ src/super_gradients/training/sg_trainer/sg_trainer.py
@@ -522,7 +522,7 @@ class Trainer:
         self.sg_logger.add_checkpoint(tag=self.ckpt_best_name, state_dict=state, global_step=epoch)
 
     # FIXME - we need to resolve flake8's 'function is too complex' for this function
-    def train(self, train_loader: DataLoader = None, valid_loader: DataLoader = None, training_params: dict = dict()):  # noqa: C901
+    def train(self, training_params: dict = dict(), train_loader: DataLoader = None, valid_loader: DataLoader = None):  # noqa: C901
         """
 
         train - Trains the Model
