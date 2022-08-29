c920cd72da3ed29f97388a1ee6d1f59d005fb2cb
Louis Dupont <louis-dupont@live.fr>
2022-08-29 13:08:11 +0300

    simplify code

--- tests/unit_tests/datalaoder_factory_test.py
+++ tests/unit_tests/datalaoder_factory_test.py
@@ -1,7 +1,6 @@
 import unittest
 
-import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 
 from super_gradients.training.dataloaders.dataloader_factory import coco2017_train, coco2017_val, \
     coco2017_train_ssd_lite_mobilenet_v2, coco2017_val_ssd_lite_mobilenet_v2, classification_test_train, detection_test_train, segmentation_test_train,\
@@ -33,22 +32,22 @@ class DataLoaderFactoryTest(unittest.TestCase):
     def test_classification_test_creation(self):
         dl = classification_test_train()
         self.assertTrue(isinstance(dl, DataLoader))
-        self.assertTrue(isinstance(dl.dataset, torch.utils.data.TensorDataset))
+        self.assertTrue(isinstance(dl.dataset, TensorDataset))
 
     def test_detection_test_creation(self):
         dl = detection_test_train()
         self.assertTrue(isinstance(dl, DataLoader))
-        self.assertTrue(isinstance(dl.dataset, torch.utils.data.TensorDataset))
+        self.assertTrue(isinstance(dl.dataset, TensorDataset))
 
     def test_segmentation_test_creation(self):
         dl = segmentation_test_train()
         self.assertTrue(isinstance(dl, DataLoader))
-        self.assertTrue(isinstance(dl.dataset, torch.utils.data.TensorDataset))
+        self.assertTrue(isinstance(dl.dataset, TensorDataset))
 
     def test_yolo_detection_test_creation(self):
         dl = yolo_detection_test_train()
         self.assertTrue(isinstance(dl, DataLoader))
-        self.assertTrue(isinstance(dl.dataset, torch.utils.data.TensorDataset))
+        self.assertTrue(isinstance(dl.dataset, TensorDataset))
 
 
 if __name__ == '__main__':
