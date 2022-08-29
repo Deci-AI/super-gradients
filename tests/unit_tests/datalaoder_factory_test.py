738ea22ca61ac0d3e6f2aec1d8144605d0de490b
Louis Dupont <louis-dupont@live.fr>
2022-08-29 09:08:55 +0300

    uncomment tests

--- tests/unit_tests/datalaoder_factory_test.py
+++ tests/unit_tests/datalaoder_factory_test.py
@@ -8,25 +8,25 @@ from super_gradients.training.datasets import COCODetectionDataset, PascalVOC201
 
 
 class DataLoaderFactoryTest(unittest.TestCase):
-    # def test_coco2017_train_creation(self):
-    #     dl_train = coco2017_train()
-    #     self.assertTrue(isinstance(dl_train, DataLoader))
-    #     self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
-    #
-    # def test_coco2017_val_creation(self):
-    #     dl_val = coco2017_val()
-    #     self.assertTrue(isinstance(dl_val, DataLoader))
-    #     self.assertTrue(isinstance(dl_val.dataset, COCODetectionDataset))
-    #
-    # def test_coco2017_train_ssdlite_mobilenet_creation(self):
-    #     dl_train = coco2017_train_ssd_lite_mobilenet_v2()
-    #     self.assertTrue(isinstance(dl_train, DataLoader))
-    #     self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
-    #
-    # def test_coco2017_val_ssdlite_mobilenet_creation(self):
-    #     dl_train = coco2017_val_ssd_lite_mobilenet_v2()
-    #     self.assertTrue(isinstance(dl_train, DataLoader))
-    #     self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
+    def test_coco2017_train_creation(self):
+        dl_train = coco2017_train()
+        self.assertTrue(isinstance(dl_train, DataLoader))
+        self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
+
+    def test_coco2017_val_creation(self):
+        dl_val = coco2017_val()
+        self.assertTrue(isinstance(dl_val, DataLoader))
+        self.assertTrue(isinstance(dl_val.dataset, COCODetectionDataset))
+
+    def test_coco2017_train_ssdlite_mobilenet_creation(self):
+        dl_train = coco2017_train_ssd_lite_mobilenet_v2()
+        self.assertTrue(isinstance(dl_train, DataLoader))
+        self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
+
+    def test_coco2017_val_ssdlite_mobilenet_creation(self):
+        dl_train = coco2017_val_ssd_lite_mobilenet_v2()
+        self.assertTrue(isinstance(dl_train, DataLoader))
+        self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
 
     def test_pascal_voc_segmentation_train_creation(self):
         dl = pascal_voc_segmentation_train()
