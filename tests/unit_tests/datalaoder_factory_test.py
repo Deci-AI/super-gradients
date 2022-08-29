28039a54e4a67e475ad027d10f10f6ec3f6c950f
Louis Dupont <louis-dupont@live.fr>
2022-08-29 11:19:20 +0300

    uncomment test

--- tests/unit_tests/datalaoder_factory_test.py
+++ tests/unit_tests/datalaoder_factory_test.py
@@ -30,25 +30,25 @@ from super_gradients.training.datasets import COCODetectionDataset, ImageNetData
 
 
 class DataLoaderFactoryTest(unittest.TestCase):
-    # def test_coco2017_train_creation(self):
-    #     dl_train = coco2017_train()
-    #     self.assertTrue(isinstance(dl_train, DataLoader))
-    #     self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
-    #
-    # def test_coco2017_val_creation(self):
-    #     dl_val = coco2017_val()
-    #     self.assertTrue(isinstance(dl_val, DataLoader))
-    #     self.assertTrue(isinstance(dl_val.dataset, COCODetectionDataset))
-    #
-    # def test_coco2017_train_ssdlite_mobilenet_creation(self):
-    #     dl_train = coco2017_train_ssd_lite_mobilenet_v2()
-    #     self.assertTrue(isinstance(dl_train, DataLoader))
-    #     self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
-    #
-    # def test_coco2017_val_ssdlite_mobilenet_creation(self):
-    #     dl_train = coco2017_val_ssd_lite_mobilenet_v2()
-    #     self.assertTrue(isinstance(dl_train, DataLoader))
-    #     self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
+    def test_coco2017_train_creation(self):
+        dl_train = coco2017_train()
+        self.assertTrue(isinstance(dl_train, DataLoader))
+        self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
+
+    def test_coco2017_val_creation(self):
+        dl_val = coco2017_val()
+        self.assertTrue(isinstance(dl_val, DataLoader))
+        self.assertTrue(isinstance(dl_val.dataset, COCODetectionDataset))
+
+    def test_coco2017_train_ssdlite_mobilenet_creation(self):
+        dl_train = coco2017_train_ssd_lite_mobilenet_v2()
+        self.assertTrue(isinstance(dl_train, DataLoader))
+        self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
+
+    def test_coco2017_val_ssdlite_mobilenet_creation(self):
+        dl_train = coco2017_val_ssd_lite_mobilenet_v2()
+        self.assertTrue(isinstance(dl_train, DataLoader))
+        self.assertTrue(isinstance(dl_train.dataset, COCODetectionDataset))
 
     def test_imagenet_train_creation(self):
         dl = imagenet_train()
