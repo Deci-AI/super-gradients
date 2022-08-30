bca248304c4e696bbee96e119b2ba920ae9fb5bf
Borys Tymchenko <tim4bor@gmail.com>
2022-08-30 15:26:28 +0300

    [SG-251] Added CIFAR10/100 dataloaders, configs and tests

--- tests/unit_tests/datalaoder_factory_test.py
+++ tests/unit_tests/datalaoder_factory_test.py
@@ -6,6 +6,10 @@ from super_gradients.training.dataloaders.dataloader_factory import (
     classification_test_dataloader,
     detection_test_dataloader,
     segmentation_test_dataloader,
+    cifar10_val,
+    cifar10_train,
+    cifar100_val,
+    cifar100_train,
     coco2017_train,
     coco2017_val,
     coco2017_train_ssd_lite_mobilenet_v2,
@@ -30,10 +34,10 @@ from super_gradients.training.dataloaders.dataloader_factory import (
     tiny_imagenet_val,
 )
 from super_gradients.training.datasets import COCODetectionDataset, ImageNetDataset
+from torchvision.datasets import CIFAR10, CIFAR100
 
 
 class DataLoaderFactoryTest(unittest.TestCase):
-
     def test_coco2017_train_creation(self):
         dl_train = coco2017_train()
         self.assertTrue(isinstance(dl_train, DataLoader))
@@ -116,7 +120,9 @@ class DataLoaderFactoryTest(unittest.TestCase):
 
     def test_imagenet_resnet50_kd_train_creation(self):
         # Here we need to overwrite the sampler because the RepeatAugSampler used in KD is only supported for DDP
-        dl = imagenet_resnet50_kd_train(dataloader_params={"sampler": {"InfiniteSampler": {}}})
+        dl = imagenet_resnet50_kd_train(
+            dataloader_params={"sampler": {"InfiniteSampler": {}}}
+        )
         self.assertTrue(isinstance(dl, DataLoader))
         self.assertTrue(isinstance(dl.dataset, ImageNetDataset))
 
@@ -145,6 +151,26 @@ class DataLoaderFactoryTest(unittest.TestCase):
         self.assertTrue(isinstance(dl, DataLoader))
         self.assertTrue(isinstance(dl.dataset, ImageNetDataset))
 
+    def test_cifar10_train_creation(self):
+        dl_train = cifar10_train()
+        self.assertTrue(isinstance(dl_train, DataLoader))
+        self.assertTrue(isinstance(dl_train.dataset, CIFAR10))
+
+    def test_cifar10_val_creation(self):
+        dl_val = cifar10_val()
+        self.assertTrue(isinstance(dl_val, DataLoader))
+        self.assertTrue(isinstance(dl_val.dataset, CIFAR10))
+
+    def test_cifar100_train_creation(self):
+        dl_train = cifar100_train()
+        self.assertTrue(isinstance(dl_train, DataLoader))
+        self.assertTrue(isinstance(dl_train.dataset, CIFAR100))
+
+    def test_cifar100_val_creation(self):
+        dl_val = cifar100_val()
+        self.assertTrue(isinstance(dl_val, DataLoader))
+        self.assertTrue(isinstance(dl_val.dataset, CIFAR100))
+
     def test_classification_test_dataloader_creation(self):
         dl = classification_test_dataloader()
         self.assertTrue(isinstance(dl, DataLoader))
@@ -161,5 +187,5 @@ class DataLoaderFactoryTest(unittest.TestCase):
         self.assertTrue(isinstance(dl.dataset, TensorDataset))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
