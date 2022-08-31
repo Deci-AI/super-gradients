f475c99fa35454d36dfee31eda3cee0f965f84a3
Louis Dupont <louis-dupont@live.fr>
2022-08-31 16:29:45 +0300

    add metrics

---
+++ src/super_gradients/training/metrics/all_metrics.py
@@ -0,0 +1,25 @@
+from super_gradients.training.metrics import Accuracy, Top5, DetectionMetrics, IoU, PixelAccuracy, BinaryIOU, Dice,\
+    BinaryDice
+
+
+class MetricNames:
+    ACCURACY = 'Accuracy'
+    TOP5 = 'Top5'
+    DETECTION_METRICS = 'DetectionMetrics'
+    IOU = 'IoU'
+    BINARY_IOU = "BinaryIOU"
+    DICE = "Dice"
+    BINARY_DICE = "BinaryDice"
+    PIXEL_ACCURACY = 'PixelAccuracy'
+
+
+METRICS = {
+    MetricNames.ACCURACY: Accuracy,
+    MetricNames.TOP5: Top5,
+    MetricNames.DETECTION_METRICS: DetectionMetrics,
+    MetricNames.IOU: IoU,
+    MetricNames.BINARY_IOU: BinaryIOU,
+    MetricNames.DICE: Dice,
+    MetricNames.BINARY_DICE: BinaryDice,
+    MetricNames.PIXEL_ACCURACY: PixelAccuracy,
+}
