75b0b236a19994b3c413f67dce0987636998e22a
Borys Tymchenko <borys.tymchenko@deci.ai>
2022-08-31 18:49:32 +0300

    [SG-251] Added end-to-end test for CIFAR100

--- tests/end_to_end_tests/__init__.py
+++ tests/end_to_end_tests/__init__.py
@@ -2,6 +2,6 @@
 
 from tests.end_to_end_tests.trainer_test import TestTrainer
 
-from tests.end_to_end_tests.cifar10_trainer_test import TestCifar10Trainer
+from tests.end_to_end_tests.cifar_trainer_test import TestCifar10Trainer
 
 __all__ = ['TestTrainer', 'TestCifar10Trainer']
