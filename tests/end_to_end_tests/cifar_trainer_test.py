aa14debde9cc605a6b6849cb370a3d20476cbf00
Borys Tymchenko <tim4bor@gmail.com>
2022-08-31 18:49:32 +0300

    [SG-251] Added end-to-end test for CIFAR100

--- tests/end_to_end_tests/cifar_trainer_test.py
+++ tests/end_to_end_tests/cifar_trainer_test.py
@@ -9,13 +9,15 @@ from super_gradients.training.datasets.dataset_interfaces import LibraryDatasetI
 from super_gradients.training.dataloaders.dataloader_factory import (
     cifar10_train,
     cifar10_val,
+    cifar100_train,
+    cifar100_val,
 )
 import torch
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 
 
-class TestCifar10Trainer(unittest.TestCase):
+class TestCifarTrainer(unittest.TestCase):
     def test_train_cifar10(self):
         super_gradients.init_trainer()
         trainer = Trainer("test", model_checkpoints_location="local")
@@ -51,6 +53,43 @@ class TestCifar10Trainer(unittest.TestCase):
             },
             train_loader=cifar10_train_dl,
             valid_loader=cifar10_val_dl,
+        )
+
+    def test_train_cifar100(self):
+        super_gradients.init_trainer()
+        trainer = Trainer("test", model_checkpoints_location="local")
+        cifar_10_dataset_interface = LibraryDatasetInterface(name="cifar100")
+        trainer.connect_dataset_interface(cifar_10_dataset_interface)
+        model = models.get("resnet18_cifar", arch_params={"num_classes": 100})
+        trainer.train(
+            model=model,
+            training_params={
+                "max_epochs": 1,
+                "initial_lr": 0.1,
+                "loss": "cross_entropy",
+                "train_metrics_list": ["Accuracy"],
+                "valid_metrics_list": ["Accuracy"],
+                "metric_to_watch": "Accuracy",
+            },
+        )
+
+    def test_train_cifar100_dataloader(self):
+        super_gradients.init_trainer()
+        trainer = Trainer("test", model_checkpoints_location="local")
+        cifar100_train_dl, cifar100_val_dl = cifar100_train(), cifar100_val()
+        model = models.get("resnet18_cifar", arch_params={"num_classes": 100})
+        trainer.train(
+            model=model,
+            training_params={
+                "max_epochs": 1,
+                "initial_lr": 0.1,
+                "loss": "cross_entropy",
+                "train_metrics_list": ["Accuracy"],
+                "valid_metrics_list": ["Accuracy"],
+                "metric_to_watch": "Accuracy",
+            },
+            train_loader=cifar100_train_dl,
+            valid_loader=cifar100_val_dl,
         )
 
 
