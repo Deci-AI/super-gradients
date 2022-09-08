3627a3e3afdc1ae2ddb5ab68836b6b051b4fd520
ofri masad <ofri@deci.ai>
2022-09-08 17:28:32 +0300

    lint

--- src/super_gradients/training/models/model_factory.py
+++ src/super_gradients/training/models/model_factory.py
@@ -18,11 +18,13 @@ from super_gradients.common.abstractions.abstract_logger import get_logger
 
 logger = get_logger(__name__)
 
+
 def get_cls(cls_path):
     module = '.'.join(cls_path.split('.')[:-1])
     name = cls_path.split('.')[-1]
     importlib.import_module(module)
     return getattr(sys.modules[module], name)
+
 
 OmegaConf.register_new_resolver("class", lambda *args: get_cls(*args))
 
@@ -138,15 +140,14 @@ class MockClient:
         self.s3 = boto3.client('s3')
         GlobalHydra.instance().clear()
 
-
-    def get_model_arch_params(self, model_name:str) -> DictConfig:
-        tmp_file_path='/home/ofri/arch_params.yaml'
+    def get_model_arch_params(self, model_name: str) -> DictConfig:
+        tmp_file_path = '/home/ofri/arch_params.yaml'
         self.s3.download_file(self.bucket_name, f'{self.prefix}/{model_name}/arch_params.yaml', tmp_file_path)
         with hydra.initialize_config_dir(config_dir='/home/ofri/'):
             cfg = hydra.compose(config_name='arch_params.yaml')
         return cfg
 
-    def get_model_weights(self, model_name:str) -> DictConfig:
-        tmp_file_path='/home/ofri/weights.pth'
+    def get_model_weights(self, model_name: str) -> DictConfig:
+        tmp_file_path = '/home/ofri/weights.pth'
         self.s3.download_file(self.bucket_name, f'{self.prefix}/{model_name}/{model_name}.pth', tmp_file_path)
         return tmp_file_path
