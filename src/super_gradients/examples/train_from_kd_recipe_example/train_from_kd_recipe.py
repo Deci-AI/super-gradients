c1404b97c8e84e8f29594103b3eaeb6c3f4e3c9a
Louis-Dupont <35190946+Louis-Dupont@users.noreply.github.com>
2022-09-11 17:05:02 +0300

    Launch DDP with script (#350)
    * first working version
    
    * wip
    
    * fix bugs - now working with n_proc_node
    
    * update recipe with nproc_per
    
    * set logging level and small updates
    
    * trying torchrun (breaking)
    
    * wip
    
    * wip[
    
    * launch_elastic working
    
    * clean version that does it all - not tested yet
    
    * cleaning code
    
    * clean branch
    
    * improve doc in run.py
    
    * add version_base to hydra.main and add comment
    
    * renaming script and removing import
    
    * flake8
    
    * wip
    
    * working version
    
    * remove unused code
    
    * update function names and comments
    
    * small update
    
    * rename nproc into n_gpus
    
    * finish renaming of gpu and small fixes
    
    * update readme
    
    * add logging when launching DDP with job info, and moving functions to training.utils.distributed...
    
    * remtrain_from_recipe_no_ddp.py becaus not needed anymore
    
    * flake8

--- src/super_gradients/examples/train_from_kd_recipe_example/train_from_kd_recipe.py
+++ src/super_gradients/examples/train_from_kd_recipe_example/train_from_kd_recipe.py
@@ -12,7 +12,7 @@ import pkg_resources
 from super_gradients.training.kd_trainer import KDTrainer
 
 
-@hydra.main(config_path=pkg_resources.resource_filename("super_gradients.recipes", ""))
+@hydra.main(config_path=pkg_resources.resource_filename("super_gradients.recipes", ""), version_base="1.2")
 def main(cfg: DictConfig) -> None:
     KDTrainer.train_from_config(cfg)
 
