bf42328dc47598653a0eed9c70af9ffaf65bd6f5
ofri masad <ofri@deci.ai>
2022-09-15 14:03:47 +0300

    fix deci client calls

--- src/super_gradients/__init__.py
+++ src/super_gradients/__init__.py
@@ -4,10 +4,11 @@ from super_gradients.common import init_trainer, is_distributed
 from super_gradients.examples.train_from_recipe_example import train_from_recipe
 from super_gradients.examples.train_from_kd_recipe_example import train_from_kd_recipe
 from super_gradients.sanity_check import env_sanity_check
+from setup import get_version
 
 __all__ = ['ARCHITECTURES', 'losses', 'utils', 'datasets_utils', 'DataAugmentation',
            'Trainer', 'KDTrainer',
            'init_trainer', 'is_distributed', 'train_from_recipe', 'train_from_kd_recipe',
-           'env_sanity_check', 'KDModel', 'SgModel']
+           'env_sanity_check', 'KDModel', 'SgModel', 'get_version']
 
 env_sanity_check()
