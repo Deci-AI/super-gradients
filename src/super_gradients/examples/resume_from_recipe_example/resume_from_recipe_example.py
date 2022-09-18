ecfc119e0bd73e347fd905b65f9afcb4fe08392c
Louis Dupont <louis-dupont@live.fr>
2022-09-18 17:45:06 +0300

    flake8

--- src/super_gradients/examples/resume_from_recipe_example/resume_from_recipe_example.py
+++ src/super_gradients/examples/resume_from_recipe_example/resume_from_recipe_example.py
@@ -4,8 +4,6 @@ Example code for running SuperGradient's recipes.
 General use: python train_from_recipe.py --config-name="DESIRED_RECIPE".
 For recipe's specific instructions and details refer to the recipe's configuration file in the recipes directory.
 """
-import argparse
-
 from omegaconf import DictConfig
 import hydra
 import pkg_resources
