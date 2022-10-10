483c6bd2211990e2ddf59226bb51a957951f5d53
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2022-10-10 15:26:06 +0300

    Make linter happy (invalid escape sequence in a comment)

--- src/super_gradients/training/utils/hydra_utils.py
+++ src/super_gradients/training/utils/hydra_utils.py
@@ -56,11 +56,11 @@ def add_params_to_cfg(cfg: DictConfig, params: List[str]):
 
 
 def normalize_path(path: str) -> str:
-    """Normalize the directory of file path. Replace the Windows-style (\) path separators with unix ones (/).
+    """Normalize the directory of file path. Replace the Windows-style (\\) path separators with unix ones (/).
     This is necessary when running on Windows since Hydra compose fails to find a configuration file is the config
     directory contains backward slash symbol.
 
     :param path: Input path string
-    :return: Output path string with all \ symbols replaces with /.
+    :return: Output path string with all \\ symbols replaces with /.
     """
     return path.replace("\\", "/")
