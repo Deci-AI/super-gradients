aba8c6473ce721f34edb111e63d40f3a60bfb202
Louis Dupont <louis-dupont@live.fr>
2022-11-02 16:35:50 +0200

    wip

--- src/super_gradients/training/sg_trainer/sg_trainer.py
+++ src/super_gradients/training/sg_trainer/sg_trainer.py
@@ -9,13 +9,14 @@ import torch
 import hydra
 from omegaconf import DictConfig
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SequentialSampler
 from torch.cuda.amp import GradScaler, autocast
 from torchmetrics import MetricCollection
 from tqdm import tqdm
 from piptools.scripts.sync import _get_installed_distributions
 
 from torch.utils.data.distributed import DistributedSampler
+from super_gradients.training.datasets.samplers import InfiniteSampler, RepeatAugSampler
 
 from super_gradients.common.factories.callbacks_factory import CallbacksFactory
 from super_gradients.common.data_types.enum import MultiGPUMode, StrictLoad, EvaluationType
@@ -928,10 +929,12 @@ class Trainer:
         self.train_loader = train_loader or self.train_loader
         self.valid_loader = valid_loader or self.valid_loader
         self._set_dataset_params()
-        if self.multi_gpu == MultiGPUMode.DISTRIBUTED_DATA_PARALLEL and not isinstance(self.train_loader.sampler, DistributedSampler):
-            raise ValueError("You are not using a DistributedSampler on you training dataloader, while working on DDP."
-                             "This cancels the DDP benefits since it makes each process iterate through the entire dataset")
 
+        if self.multi_gpu == MultiGPUMode.DISTRIBUTED_DATA_PARALLEL:
+            if isinstance(self.train_loader.sampler, SequentialSampler):
+                raise ValueError("You are not using a DistributedSampler on you training dataloader, while working on DDP."
+                                 "This cancels the DDP benefits since it makes each process iterate through the entire dataset")
+            if not isinstance(self.train_loader.sampler, )
         self.training_params = TrainingParams()
         self.training_params.override(**training_params)
 
