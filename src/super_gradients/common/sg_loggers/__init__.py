26a26911a2c716e62d5604644119edd593382c9f
Louis Dupont <louis-dupont@live.fr>
2022-11-06 15:00:09 +0200

    black

--- src/super_gradients/common/sg_loggers/__init__.py
+++ src/super_gradients/common/sg_loggers/__init__.py
@@ -3,7 +3,9 @@ from super_gradients.common.sg_loggers.deci_platform_sg_logger import DeciPlatfo
 from super_gradients.common.sg_loggers.wandb_sg_logger import WandBSGLogger
 from super_gradients.common.sg_loggers.clearml_sg_logger import ClearMLSGLogger
 
-SG_LOGGERS = {'base_sg_logger': BaseSGLogger,
-              'deci_platform_sg_logger': DeciPlatformSGLogger,
-              'wandb_sg_logger': WandBSGLogger,
-              'clearml_sg_logger': ClearMLSGLogger}
+SG_LOGGERS = {
+    "base_sg_logger": BaseSGLogger,
+    "deci_platform_sg_logger": DeciPlatformSGLogger,
+    "wandb_sg_logger": WandBSGLogger,
+    "clearml_sg_logger": ClearMLSGLogger,
+}
