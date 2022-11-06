6398590f77b492f6a6516d676a2e0bf06556ca3c
Louis Dupont <louis-dupont@live.fr>
2022-11-06 14:57:51 +0200

    remove unwanted change

--- src/super_gradients/training/utils/callbacks/all_callbacks.py
+++ src/super_gradients/training/utils/callbacks/all_callbacks.py
@@ -12,8 +12,6 @@ from super_gradients.training.utils.callbacks.callbacks import (
     ExponentialLRCallback,
     FunctionLRCallback,
     WarmupLRCallback,
-    BinarySegmentationVisualizationCallback,
-    DetectionVisualizationCallback,
 )
 
 from super_gradients.training.utils.early_stopping import EarlyStop
@@ -29,8 +27,6 @@ CALLBACKS = {
     Callbacks.EARLY_STOP: EarlyStop,
     Callbacks.DETECTION_MULTISCALE_PREPREDICTION: DetectionMultiscalePrePredictionCallback,
     Callbacks.YOLOX_TRAINING_STAGE_SWITCH: YoloXTrainingStageSwitchCallback,
-    Callbacks.DETECTION_VISUALIZATION: DetectionVisualizationCallback,
-    Callbacks.BINARY_SEGMENTATION_VISUALIZATION: BinarySegmentationVisualizationCallback,
 }
 
 
