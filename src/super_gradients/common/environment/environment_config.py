177711890cbb8bad0844b55c5a14f8dbf392d2ea
Louis Dupont <louis-dupont@live.fr>
2022-11-13 10:07:23 +0200

    clean and set dir name

--- src/super_gradients/common/environment/environment_config.py
+++ src/super_gradients/common/environment/environment_config.py
@@ -5,11 +5,8 @@ import pkg_resources
 try:
     PKG_CHECKPOINTS_DIR = pkg_resources.resource_filename("checkpoints", "")
 except Exception:
-    PKG_CHECKPOINTS_DIR = os.path.join(os.path.expanduser("~/sg_logs/"), "checkpoints")
+    PKG_CHECKPOINTS_DIR = os.path.join(os.path.expanduser("~/sg/"), "checkpoints")
     os.makedirs(PKG_CHECKPOINTS_DIR, exist_ok=True)
-    # os.makedirs(os.path.join(os.getcwd(), "checkpoints"), exist_ok=True) # FIXME this creates in the working dir which is different when running
-    #                                                                      # train_from_config and evaluate_from_config
-
 
 DDP_LOCAL_RANK = -1
 
