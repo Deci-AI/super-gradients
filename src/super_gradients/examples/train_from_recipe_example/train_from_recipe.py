b7b948c2e7db8b23d14bb7bede22a5fd766cf1bc
Eugene <ekhvedchenya@gmail.com>
2022-11-16 23:15:48 +0200

    Added try/catch to log exceptions to file

--- src/super_gradients/examples/train_from_recipe_example/train_from_recipe.py
+++ src/super_gradients/examples/train_from_recipe_example/train_from_recipe.py
@@ -10,11 +10,17 @@ import hydra
 import pkg_resources
 
 from super_gradients import Trainer, init_trainer
+from super_gradients.common.abstractions.abstract_logger import get_logger
+
+logger = get_logger(__name__)
 
 
 @hydra.main(config_path=pkg_resources.resource_filename("super_gradients.recipes", ""), version_base="1.2")
 def main(cfg: DictConfig) -> None:
-    Trainer.train_from_config(cfg)
+    try:
+        Trainer.train_from_config(cfg)
+    except Exception as err:
+        logger.exception("Encountered exception during training: {}".format(err))
 
 
 def run():
