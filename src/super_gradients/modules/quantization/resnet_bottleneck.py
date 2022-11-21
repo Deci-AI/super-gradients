b143801bf5019801281cffdf9dfc0abcdf0279df
Borys Tymchenko <tim4bor@gmail.com>
2022-11-21 16:05:37 +0200

    Removed redundand get metadata for source

--- src/super_gradients/modules/quantization/resnet_bottleneck.py
+++ src/super_gradients/modules/quantization/resnet_bottleneck.py
@@ -1,12 +1,8 @@
-from typing import Union, Type
-
 from super_gradients.training.models import Bottleneck
 
 try:
     from pytorch_quantization import nn as quant_nn
-    from pytorch_quantization.tensor_quant import QuantDescriptor
-
-    from super_gradients.training.utils.quantization.core import SGQuantMixin, QuantizedMetadata
+    from super_gradients.training.utils.quantization.core import SGQuantMixin
 
     _imported_pytorch_quantization_failure = False
 
@@ -28,12 +24,3 @@ class QuantBottleneck(SGQuantMixin):
     def from_float(cls, float_instance: Bottleneck, **kwargs):
         float_instance.shortcut.add_module("residual_quantizer", quant_nn.TensorQuantizer(kwargs.get("quant_desc_input")))
         return float_instance
-
-    @staticmethod
-    def get_quantized_metadata_for_source(float_source: Union[str, Type]):
-        return QuantizedMetadata(
-            float_source=float_source,
-            quantized_target_class=QuantBottleneck,
-            input_quant_descriptor=QuantDescriptor(calib_method="histogram"),
-            action=QuantizedMetadata.ReplacementAction.RECURE_AND_REPLACE,
-        )
