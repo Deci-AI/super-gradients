95fe98003c6cac95d7bba385d14bd9282dec7e01
Borys Tymchenko <tim4bor@gmail.com>
2022-11-21 13:44:08 +0200

    Added import guard on ion quantized resnet bottleneck

--- src/super_gradients/modules/quantization/resnet_bottleneck.py
+++ src/super_gradients/modules/quantization/resnet_bottleneck.py
@@ -1,10 +1,17 @@
 from typing import Union, Type
 
-from pytorch_quantization.tensor_quant import QuantDescriptor
-
 from super_gradients.training.models import Bottleneck
-from super_gradients.training.utils.quantization.core import SGQuantMixin, QuantizedMetadata
-from pytorch_quantization import nn as quant_nn
+
+try:
+    from pytorch_quantization import nn as quant_nn
+    from pytorch_quantization.tensor_quant import QuantDescriptor
+
+    from super_gradients.training.utils.quantization.core import SGQuantMixin, QuantizedMetadata
+
+    _imported_pytorch_quantization_failure = False
+
+except (ImportError, NameError, ModuleNotFoundError):
+    _imported_pytorch_quantization_failure = True
 
 
 class QuantBottleneck(SGQuantMixin):
@@ -13,6 +20,9 @@ class QuantBottleneck(SGQuantMixin):
     NOTE: we must quantize the float instance, so the mode should be
           QuantizedMetadata.ReplacementAction.QUANTIZE_CHILDREN_THEN_REPLACE
     """
+
+    if _imported_pytorch_quantization_failure is not None:
+        raise _imported_pytorch_quantization_failure
 
     @classmethod
     def from_float(cls, float_instance: Bottleneck, **kwargs):
