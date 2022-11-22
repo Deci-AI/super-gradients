cc3b992db814e52ed2fa11c73a05bce806c0080c
Natan Bagrov <NatanBagrov@users.noreply.github.com>
2022-11-22 17:00:31 +0200

    Feature/sg 000 qat (#304)
    * initial progress with fine-grain quantization
    
    * refactoring and cleanup. wip
    
    * refactoring and cleanup. wip
    
    * wip, added partial tests
    
    * core progress with selective quantization utility. added tests
    
    * removed redundant file
    
    * sync with master branch
    
    * sync with master branch
    
    * fix ci
    
    * fix lint
    
    * added support for cases where pytorch_quantization is not installed
    
    * added dynamic mapping, and test
    
    * added tests for custom quant descriptors
    
    * more flexibility to custom quantized classes
    
    * fix skip import
    
    * minor renaming, added examples, added calibrator, added export, fixed minor bug with proper device allocation
    
    * remove unused imports
    
    * minor comment
    
    * verbose info when doing calibration
    
    * more robust device tracking
    added option to preserve state dict
    added more default quant mappings
    
    * added more flexibility to replacement
    
    * wip
    
    * refactoring
    
    * wip
    
    * Transition to SG 3.0 and fix of default parameters to working with SGResNet50 model
    
    * Make black and flake8 happy, at leeast partially
    
    * Make black happier
    
    * Guarded imports of pytorch_quantization in the quantization core
    
    * Guarded import of QATCallback in the trainer
    
    * Guarded imports of quantization-related stuff
    
    * Removed QATCallback for now
    
    * Correctly restore state of use_fb_fake_quant during export
    
    * Added pytorch-quantization and nvidia index to requirements.txt
    
    * Added nvidia index as extra index to requirements.txt
    
    * Removed extra nvidia index as snyk is not happy about it
    
    * Updated calibrator to work on the same device as model
    
    * Added kwargs to prep_model_for_conversion
    
    * Updated resnet50 QAT example, for now without integration
    
    * Some works in the examples department
    
    * Added decorator mapping example
    
    * Added pytorch-quantization to requirements.txt
    
    * Fiddle with pytorch-quantization to requirements.txt
    
    * Fiddle with pytorch-quantization to requirements.txt
    
    * Fiddle with pytorch-quantization to requirements.txt
    
    * fixed an issue with nesting not being propagated correctly, resulting mappings not applied to nested layer names
    
    * Removed unneeded quantized resnet version and corresponding test as they are redundant
    
    * Moved quantized resnet bottleneck to modules.quantization
    
    * Added import guard on ion quantized resnet bottleneck
    
    * Removed QuantizedMetadata.from_dict method
    
    * Gave shorter names to QuantizedMetadata.ReplacementAction
    
    * Removed redundand get metadata for source
    
    * Completed renaming of QuantizedMetadata.ReplacementAction
    
    * fix requirement
    
    * fix requirement
    
    * fix requirement
    
    * fix ci
    
    * Rewritten RegisterQuantizedModule as a decorator to be consistent with other registries
    
    * Removed outdated quantization_utils.py
    
    * Regitsered QuantBottleneck
    
    Co-authored-by: Borys Tymchenko <tim4bor@gmail.com>
    Co-authored-by: ofri masad <ofri@deci.ai>
    Co-authored-by: Ofri Masad <ofrimasad@users.noreply.github.com>
    Co-authored-by: Shay Aharon <80472096+shaydeci@users.noreply.github.com>

---
+++ src/super_gradients/modules/quantization/resnet_bottleneck.py
@@ -0,0 +1,27 @@
+from super_gradients.training.models import Bottleneck
+
+try:
+    from pytorch_quantization import nn as quant_nn
+    from super_gradients.training.utils.quantization.core import SGQuantMixin, QuantizedMetadata
+    from super_gradients.training.utils.quantization.selective_quantization_utils import register_quantized_module
+
+    _imported_pytorch_quantization_failure = None
+except (ImportError, NameError, ModuleNotFoundError) as import_err:
+    _imported_pytorch_quantization_failure = import_err
+
+
+@register_quantized_module(float_source=Bottleneck, action=QuantizedMetadata.ReplacementAction.RECURE_AND_REPLACE)
+class QuantBottleneck(SGQuantMixin):
+    """
+    we just insert quantized tensor to the shortcut (=residual) layer, so that it would be quantized
+    NOTE: we must quantize the float instance, so the mode should be
+          QuantizedMetadata.ReplacementAction.RECURE_AND_REPLACE
+    """
+
+    if _imported_pytorch_quantization_failure is not None:
+        raise _imported_pytorch_quantization_failure
+
+    @classmethod
+    def from_float(cls, float_instance: Bottleneck, **kwargs):
+        float_instance.shortcut.add_module("residual_quantizer", quant_nn.TensorQuantizer(kwargs.get("quant_desc_input")))
+        return float_instance
