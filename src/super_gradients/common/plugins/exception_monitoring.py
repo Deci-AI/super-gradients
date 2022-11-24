6dfd56e8d7a37af75fd12ac91c9d7a35361bb987
Louis Dupont <louis-dupont@live.fr>
2022-11-24 17:22:16 +0200

    move ExceptionInfo to crash_handler

--- src/super_gradients/common/plugins/exception_monitoring.py
+++ src/super_gradients/common/plugins/exception_monitoring.py
@@ -1,12 +1,11 @@
 import os
-import sys
 import logging
 import atexit
-from typing import Callable
 
+from super_gradients.common import is_distributed
 from super_gradients.common.auto_logging.console_logging import ConsoleSink
 from super_gradients.common.environment.env_helpers import multi_process_safe
-from super_gradients.common import is_distributed
+from super_gradients.common.crash_handler.exception import ExceptionInfo
 
 try:
     from deci_lab_client.client import DeciPlatformClient
@@ -19,32 +18,6 @@ except (ImportError, NameError, ModuleNotFoundError) as _import_err:
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-
-class ExceptionInfo:
-    """Holds information about the session exception (if any)"""
-
-    _is_exception_raised = False
-
-    @staticmethod
-    def register_exception():
-        """Register the exception information into the class"""
-        ExceptionInfo._is_exception_raised = True
-
-    @staticmethod
-    def is_exception_raised():
-        """Check if an exception was raised in the current process"""
-        return ExceptionInfo._is_exception_raised
-
-
-def register_exceptions(excepthook: Callable):
-    """Wrap excepthook with a step the saves the exception info to be available in the exit hooks."""
-
-    def excepthook_with_register(exc_type, exc_value, exc_traceback):
-        ExceptionInfo.register_exception()
-        return excepthook(exc_type, exc_value, exc_traceback)
-
-    return excepthook_with_register
 
 
 @multi_process_safe
@@ -80,7 +53,6 @@ def setup_pro_user_monitoring():
         platform_client.login(token=os.getenv("DECI_PLATFORM_TOKEN"))
         logger.info("Connection to the deci platform successful!")
 
-        sys.excepthook = register_exceptions(sys.excepthook)
         atexit.register(exception_upload_handler, platform_client)
     else:
         logger.info("Automatic log upload was disabled. To enable it please set the env variable UPLOAD_LOGS=TRUE")
