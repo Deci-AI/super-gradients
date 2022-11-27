2adb2e49745d597813dc8665e782beb172d41aa8
Louis-Dupont <35190946+Louis-Dupont@users.noreply.github.com>
2022-11-27 14:45:10 +0200

    Crash tip (#493)
    * first working examples - still requries cleaning
    
    * first well packaged version
    
    * add new crash tip
    
    * add test for DDPNotInitialized
    
    * clean tests and some names
    
    * add doc
    
    * clean
    
    * small txt change
    
    * imrpove display
    
    * small format fix
    
    * clean comments
    
    * improve crash tip for cuda
    
    * move ExceptionInfo to crash_handler
    
    * reorganise the code
    
    * reorganise the code
    
    * fix import issues
    
    * fix ignore condition
    
    Co-authored-by: Ofri Masad <ofrimasad@users.noreply.github.com>

---
+++ src/super_gradients/common/crash_handler/crash_tips_setup.py
@@ -0,0 +1,24 @@
+import os
+import atexit
+
+from super_gradients.common.crash_handler.exception import ExceptionInfo
+from super_gradients.common.abstractions.abstract_logger import get_logger
+
+logger = get_logger(__name__)
+
+
+def crash_tip_handler():
+    """Display a crash tip if an error was raised"""
+    crash_tip_message = ExceptionInfo.get_crash_tip_message()
+    if crash_tip_message:
+        print(crash_tip_message)
+
+
+def setup_crash_tips() -> bool:
+    if os.getenv("CRASH_HANDLER", "TRUE") != "FALSE":
+        logger.info("Crash tips is enabled. You can set your environment variable to CRASH_HANDLER=FALSE to disable it")
+        atexit.register(crash_tip_handler)
+        return True
+    else:
+        logger.info("Crash tips is disabled. You can set your environment variable to CRASH_HANDLER=TRUE to enable it")
+        return False
