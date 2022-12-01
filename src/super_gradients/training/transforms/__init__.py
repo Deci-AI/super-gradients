0048206464131cb0f98d27e4ca7ee33ac00bb2d8
Shay Aharon <80472096+shaydeci@users.noreply.github.com>
2022-12-01 10:05:49 +0200

    Feature/sg 416 albumentations plugin for classification (#495)
    * fix: More supported types in MultiOutputModule
    
    * fix: In channels in MobileNetV1
    
    * fix: MobileNetV2 in backbone mode
    
    * feature: Open arch_params with hydra
    
    * feature: Add SSD detection modules
    
    * refactor: New SSD definition
    
    * docs: More SSD docstrings
    
    * fix: Resolve circular imports
    
    * fix: Replace head in detection base class
    
    * refactor: Better yaml loading function
    
    * fix: pre-trained test SSD arch_params navigation
    
    * refactor: Black
    
    * flake
    
    * requirements updated
    
    * formatting
    
    * flake8 formatting
    
    * optional import fixed in integration test
    
    * base compose tranforms in all transfroms fixed
    
    * albumentations cifar10 example dataset params added formatting
    
    Co-authored-by: yurkovak <katedayreen@gmail.com>
    Co-authored-by: Kate Yurkova <kate.yurkova@deci.ai>
    Co-authored-by: Eugene Khvedchenya <ekhvedchenya@gmail.com>

--- src/super_gradients/training/transforms/__init__.py
+++ src/super_gradients/training/transforms/__init__.py
@@ -1,10 +1,31 @@
 # PACKAGE IMPORTS FOR EXTERNAL USAGE
 import cv2
-from super_gradients.training.transforms.transforms import DetectionMosaic, DetectionRandomAffine, DetectionHSV,\
-    DetectionPaddedRescale, DetectionTargetsFormatTransform
-from super_gradients.training.transforms.all_transforms import TRANSFORMS, Transforms
+from super_gradients.training.transforms.transforms import (
+    DetectionMosaic,
+    DetectionRandomAffine,
+    DetectionHSV,
+    DetectionPaddedRescale,
+    DetectionTargetsFormatTransform,
+)
+from super_gradients.training.transforms.all_transforms import (
+    TRANSFORMS,
+    ALBUMENTATIONS_TRANSFORMS,
+    Transforms,
+    ALBUMENTATIONS_COMP_TRANSFORMS,
+    imported_albumentations_failure,
+)
 
-__all__ = ['TRANSFORMS', 'Transforms', 'DetectionMosaic', 'DetectionRandomAffine', 'DetectionHSV', 'DetectionPaddedRescale',
-           'DetectionTargetsFormatTransform']
+__all__ = [
+    "TRANSFORMS",
+    "ALBUMENTATIONS_TRANSFORMS",
+    "ALBUMENTATIONS_COMP_TRANSFORMS",
+    "Transforms",
+    "DetectionMosaic",
+    "DetectionRandomAffine",
+    "DetectionHSV",
+    "DetectionPaddedRescale",
+    "DetectionTargetsFormatTransform",
+    "imported_albumentations_failure",
+]
 
 cv2.setNumThreads(0)
