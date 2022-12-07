28ec3942e197dde63dea71d8121fd218b7bd8e0e
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2022-12-07 12:20:13 +0200

    Update tests

--- tests/unit_tests/config_inspector_test.py
+++ tests/unit_tests/config_inspector_test.py
@@ -1,11 +1,11 @@
 import copy
+import os
 import unittest
 
+import pkg_resources
 from omegaconf import OmegaConf
 
-from super_gradients.training.models import SgModule
-from super_gradients.training.models.all_architectures import ARCHITECTURES
-
+from super_gradients.training.models import SgModule, get_arch_params
 from super_gradients.training.models.model_factory import get_architecture
 from super_gradients.training.utils import HpmStruct
 from super_gradients.training.utils.config_utils import raise_if_unused_params, UnusedConfigParamException, AccessCounterDict, AccessCounterHpmStruct
@@ -208,26 +208,49 @@ class ConfigInspectTest(unittest.TestCase):
             },
         )
 
-    def test_model_architectures(self):
-        for model_name in ARCHITECTURES.keys():
-            architecture_cls, arch_params, pretrained_weights_path, is_remote = get_architecture(model_name, HpmStruct())
-            self.assertIsNotNone(arch_params, msg=model_name)
+    def get_all_arch_params_configs(self):
+        config_path = pkg_resources.resource_filename("super_gradients.recipes", "arch_params")
+        configs = [path.replace(".yaml", "") for path in sorted(os.listdir(config_path)) if path.endswith(".yaml")]
+        return configs
 
-            if not issubclass(architecture_cls, SgModule):
-                # net = architecture_cls(**arch_params.to_dict(include_schema=False))
-                pass
-            else:
-                # Most of the SG models work with a single params names "arch_params" of type HpmStruct, but a few take **kwargs instead
-                if "arch_params" not in get_callable_param_names(architecture_cls):
+    def test_resnet18_cifar_arch_params(self):
+        arch_params = get_arch_params("resnet18_cifar_arch_params")
+        architecture_cls, arch_params, pretrained_weights_path, is_remote = get_architecture("resnet18", HpmStruct(**arch_params))
+
+        with raise_if_unused_params(arch_params) as tracked_arch_params:
+            _ = architecture_cls(arch_params=tracked_arch_params)
+
+        with self.assertRaisesRegex(UnusedConfigParamException, "Detected unused parameters in configuration object that were not consumed by caller"):
+            arch_params.override(me_is_not_used=True)
+            with raise_if_unused_params(arch_params) as tracked_arch_params:
+                _ = architecture_cls(arch_params=tracked_arch_params)
+
+    @unittest.expectedFailure
+    def test_model_from_arch_params(self):
+        all_configs = self.get_all_arch_params_configs()
+        for config_name in all_configs:
+            with self.subTest(config_name):
+                model_name = config_name.replace("_arch_params", "")
+                arch_params = get_arch_params(config_name)
+                architecture_cls, arch_params, pretrained_weights_path, is_remote = get_architecture(model_name, HpmStruct(**arch_params))
+                self.assertIsNotNone(arch_params, msg=model_name)
+
+                if not issubclass(architecture_cls, SgModule):
+                    # This instantiation method is not supported as unpacking arch_params would cause root params to be considered "used"
                     # net = architecture_cls(**arch_params.to_dict(include_schema=False))
-                    pass
+                    self.skipTest("Skipping test since model class is not subclass of SgModule")
                 else:
-                    with self.subTest(model_name):
+                    # Most of the SG models work with a single params names "arch_params" of type HpmStruct, but a few take **kwargs instead
+                    if "arch_params" not in get_callable_param_names(architecture_cls):
+                        self.skipTest("Skipping test since model c'tor does not receive arch_params argument")
+                        # This instantiation method is not supported as unpacking arch_params would cause root params to be considered "used"
+                        # net = architecture_cls(**arch_params.to_dict(include_schema=False))
+                        pass
+                    else:
                         try:
-                            _ = architecture_cls(arch_params=arch_params, num_classes=10)
-                        except Exception:
-                            self.skipTest("Skipping since model cannot be instantiated with YAML configuration")
-                            pass
+                            _ = architecture_cls(arch_params=arch_params)
+                        except Exception as e:
+                            self.skipTest(f"Skipping test since model cannot be instantiated at all {e}")
 
                         with raise_if_unused_params(arch_params) as tracked_arch_params:
-                            _ = architecture_cls(arch_params=tracked_arch_params, num_classes=10)
+                            _ = architecture_cls(arch_params=tracked_arch_params)
