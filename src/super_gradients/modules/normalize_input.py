9307ed71087614b6563fb19a1975303b32782ec8
Eugene <ekhvedchenya@gmail.com>
2022-12-09 14:26:08 +0200

    Fixed to make evaluation running

--- src/super_gradients/modules/normalize_input.py
+++ src/super_gradients/modules/normalize_input.py
@@ -9,8 +9,9 @@ class NormalizeInput(nn.Module):
         super().__init__()
         mean = list(map(float, mean))
         std = list(map(float, std))
-        self.register_buffer("mean", torch.tensor(mean).float().reshape(1, len(mean), 1, 1).contiguous())
-        self.register_buffer("one_over_std", torch.tensor(std).float().reshape(1, len(std), 1, 1).reciprocal().contiguous())
+        # TODO: Maybe ise persistent=True
+        self.register_buffer("mean", torch.tensor(mean).float().reshape(1, len(mean), 1, 1).contiguous(), persistent=False)
+        self.register_buffer("one_over_std", torch.tensor(std).float().reshape(1, len(std), 1, 1).reciprocal().contiguous(), persistent=False)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         return (input - self.mean) * self.one_over_std
