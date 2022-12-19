57c077d2e8c46ad9e867e4960b6e117a6c11acee
shayaharon <shay.aharon@deci.ai>
2022-12-19 14:00:30 +0200

    workflow added

---
+++ tests/recipe_training_tests/cifar10_recipe_sanity_test.py
@@ -0,0 +1,20 @@
+from omegaconf import DictConfig
+import hydra
+import pkg_resources
+
+from super_gradients import Trainer, init_trainer
+
+
+@hydra.main(config_path=pkg_resources.resource_filename("super_gradients.recipes", ""), config_name="cifar10_resnet", version_base="1.2")
+def main(cfg: DictConfig) -> None:
+    cfg["training_hyperparams"]["max_epochs"] = 10
+    Trainer.train_from_config(cfg)
+
+
+def run():
+    init_trainer()
+    main()
+
+
+if __name__ == "__main__":
+    run()
