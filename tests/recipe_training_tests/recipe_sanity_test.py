56ccad18354c9e984223d38b94e0bd6eef01806e
shayaharon <shay.aharon@deci.ai>
2022-12-26 11:28:11 +0200

    assert with abs, cifar rolled back

--- tests/recipe_training_tests/recipe_sanity_test.py
+++ tests/recipe_training_tests/recipe_sanity_test.py
@@ -9,18 +9,19 @@ from super_gradients.common.environment.ddp_utils import multi_process_safe
 
 
 @multi_process_safe
-def _assert_recipe_metric(experiment_name: str, metric_value: float):
+def _assert_recipe_metric(experiment_name: str, metric_value: float, delta: float):
     ckpt_dir = os.path.join(environment_config.PKG_CHECKPOINTS_DIR, experiment_name)
     sd = torch.load(os.path.join(ckpt_dir, "ckpt_best.pth"))
-    assert sd["acc"].cpu().item() >= metric_value
+    assert abs(sd["acc"].cpu().item() - metric_value) <= delta
 
 
 @hydra.main(config_path=pkg_resources.resource_filename("super_gradients.recipes", ""), config_name="cifar10_resnet", version_base="1.2")
 def main(cfg: DictConfig) -> None:
     goal_metric_val = cfg["goal_metric_val"]
     experiment_name = cfg["experiment_name"]
+    delta = cfg["delta"]
     Trainer.train_from_config(cfg)
-    _assert_recipe_metric(experiment_name, goal_metric_val)
+    _assert_recipe_metric(experiment_name, goal_metric_val, delta)
 
 
 def run():
