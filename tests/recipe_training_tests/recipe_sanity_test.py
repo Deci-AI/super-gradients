ea816dcf7ad75d0cc5bac211820aaeb489639442
shayaharon <shay.aharon@deci.ai>
2022-12-26 12:12:59 +0200

    formatter

--- tests/recipe_training_tests/recipe_sanity_test.py
+++ tests/recipe_training_tests/recipe_sanity_test.py
@@ -7,20 +7,28 @@ import torch
 from super_gradients import Trainer, init_trainer
 from super_gradients.common.environment.ddp_utils import multi_process_safe
 from super_gradients.common.abstractions.abstract_logger import get_logger
+from super_gradients.training.utils.distributed_training_utils import wait_for_the_master, get_local_rank
 
 logger = get_logger(__name__)
 
 
-@multi_process_safe
 def _assert_recipe_metric(experiment_name: str, metric_value: float, delta: float):
     ckpt_dir = os.path.join(environment_config.PKG_CHECKPOINTS_DIR, experiment_name)
     sd = torch.load(os.path.join(ckpt_dir, "ckpt_best.pth"))
     metric_val_reached = sd["acc"].cpu().item()
     diff = abs(metric_val_reached - metric_value)
+    _print_test_result(delta, diff, metric_val_reached, metric_value)
+    if diff <= delta:
+        exit(0)
+    else:
+        exit(1)
+
+
+@multi_process_safe
+def _print_test_result(delta, diff, metric_val_reached, metric_value):
     logger.info(
         "Goal metric value: " + str(metric_value) + ", metric value reached: " + str(metric_val_reached) + ",diff: " + str(diff) + ", delta: " + str(delta)
     )
-    assert diff <= delta
 
 
 @hydra.main(config_path=pkg_resources.resource_filename("super_gradients.recipes", ""), config_name="cifar10_resnet", version_base="1.2")
@@ -29,7 +37,8 @@ def main(cfg: DictConfig) -> None:
     experiment_name = cfg["experiment_name"]
     delta = cfg["delta"]
     Trainer.train_from_config(cfg)
-    _assert_recipe_metric(experiment_name, goal_metric_val, delta)
+    with wait_for_the_master(get_local_rank()):
+        _assert_recipe_metric(experiment_name, goal_metric_val, delta)
 
 
 def run():
