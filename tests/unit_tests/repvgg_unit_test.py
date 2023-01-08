d7d2b457070fe36647416bfeac35dc7aa2c72017
shayaharon <shay.aharon@deci.ai>
2023-01-08 11:45:27 +0200

    toerance fix repvgg unit test

--- tests/unit_tests/repvgg_unit_test.py
+++ tests/unit_tests/repvgg_unit_test.py
@@ -72,11 +72,7 @@ class TestRepVgg(unittest.TestCase):
 
             deployment_mode_output = model(test_input)
             # difference is of very low magnitude
-            if False in torch.isclose(training_mode_output, deployment_mode_output, atol=1e-6):
-                print("training_mode_output: " + str(training_mode_output))
-                print("deployment_mode_output: " + str(deployment_mode_output))
-                print("coparison: " + str(torch.isclose(training_mode_output, deployment_mode_output, atol=1e-6)))
-            self.assertFalse(False in torch.isclose(training_mode_output, deployment_mode_output, atol=1e-6))
+            self.assertFalse(False in torch.isclose(training_mode_output, deployment_mode_output, atol=1e-4))
 
     def test_backbone_mode(self):
         """
