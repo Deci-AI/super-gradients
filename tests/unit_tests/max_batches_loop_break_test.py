f3a9efb75cb97491f939750672030006a86e8e90
shayaharon <shay.aharon@deci.ai>
2023-01-12 12:48:05 +0200

    test adde for validation

--- tests/unit_tests/max_batches_loop_break_test.py
+++ tests/unit_tests/max_batches_loop_break_test.py
@@ -16,7 +16,7 @@ class LastBatchIdxCollector(PhaseCallback):
         self.last_batch_idx = context.batch_idx
 
 
-class MaxTrainBatchesBreakTest(unittest.TestCase):
+class MaxBatchesLoopBreakTest(unittest.TestCase):
     def test_max_train_batches_loop_break(self):
         last_batch_collector = LastBatchIdxCollector()
         train_params = {
@@ -40,7 +40,42 @@ class MaxTrainBatchesBreakTest(unittest.TestCase):
 
         # Define Model
         net = LeNet()
-        trainer = Trainer("test_max_batches_break")
+        trainer = Trainer("test_max_batches_break_train")
+        trainer.train(
+            model=net,
+            training_params=train_params,
+            train_loader=classification_test_dataloader(dataset_size=16, batch_size=4),
+            valid_loader=classification_test_dataloader(),
+        )
+
+        # ASSERT LAST BATCH IDX IS 2
+        print(last_batch_collector.last_batch_idx)
+        self.assertTrue(last_batch_collector.last_batch_idx == 2)
+
+    def test_max_valid_batches_loop_break(self):
+        last_batch_collector = LastBatchIdxCollector(train=False)
+        train_params = {
+            "max_epochs": 2,
+            "lr_updates": [1],
+            "lr_decay_factor": 0.1,
+            "lr_mode": "step",
+            "lr_warmup_epochs": 0,
+            "initial_lr": 0.1,
+            "loss": "cross_entropy",
+            "optimizer": "SGD",
+            "criterion_params": {},
+            "optimizer_params": {"weight_decay": 1e-4, "momentum": 0.9},
+            "train_metrics_list": [Accuracy(), Top5()],
+            "valid_metrics_list": [Accuracy(), Top5()],
+            "metric_to_watch": "Accuracy",
+            "greater_metric_to_watch_is_better": True,
+            "phase_callbacks": [last_batch_collector],
+            "max_valid_batches": 3,
+        }
+
+        # Define Model
+        net = LeNet()
+        trainer = Trainer("test_max_batches_break_val")
         trainer.train(
             model=net,
             training_params=train_params,
