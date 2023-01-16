9ae5a60afe8dc1e99eb818862d96d9654e0b9c73
Louis Dupont <louis-dupont@live.fr>
2023-01-16 13:26:15 +0200

    first draft

--- src/super_gradients/training/sg_trainer/sg_trainer.py
+++ src/super_gradients/training/sg_trainer/sg_trainer.py
@@ -585,7 +585,7 @@ class Trainer:
         if (metric > self.best_metric and self.greater_metric_to_watch_is_better) or (metric < self.best_metric and not self.greater_metric_to_watch_is_better):
             # STORE THE CURRENT metric AS BEST
             self.best_metric = metric
-            self._save_best_checkpoint(epoch, state)
+            self.sg_logger.add_checkpoint(tag=self.ckpt_best_name, state_dict=state, global_step=epoch)
 
             # RUN PHASE CALLBACKS
             self.phase_callback_handler(Phase.VALIDATION_END_BEST_EPOCH, context)
@@ -595,12 +595,10 @@ class Trainer:
             logger.info("Best checkpoint overriden: validation " + self.metric_to_watch + ": " + str(metric))
 
         if self.training_params.average_best_models:
-            net_for_averaging = self.ema_model.ema if self.ema else self.net
-            averaged_model_sd = self.model_weight_averaging.get_average_model(net_for_averaging, validation_results_tuple=validation_results_tuple)
-            self.sg_logger.add_checkpoint(tag=self.average_model_checkpoint_filename, state_dict={"net": averaged_model_sd}, global_step=epoch)
-
-    def _save_best_checkpoint(self, epoch, state):
-        self.sg_logger.add_checkpoint(tag=self.ckpt_best_name, state_dict=state, global_step=epoch)
+            state["net"] = self.model_weight_averaging.get_average_model(self.net, validation_results_tuple=validation_results_tuple)
+            if self.ema:
+                state["ema_net"] = self.model_weight_averaging.get_average_model(self.ema_model.ema, validation_results_tuple=validation_results_tuple)
+            self.sg_logger.add_checkpoint(tag=self.average_model_checkpoint_filename, state_dict=state, global_step=epoch)
 
     def _prep_net_for_train(self):
         if self.arch_params is None:
--- src/super_gradients/training/utils/checkpoint_utils.py
+++ src/super_gradients/training/utils/checkpoint_utils.py
@@ -216,7 +216,7 @@ def load_checkpoint_to_model(
     checkpoint = read_ckpt_state_dict(ckpt_path=ckpt_local_path)
 
     if load_ema_as_net:
-        if "ema_net" not in checkpoint.keys():
+        if checkpoint.get("ema_net") is not None:
             raise ValueError("Can't load ema network- no EMA network stored in checkpoint file")
         else:
             checkpoint["net"] = checkpoint["ema_net"]
