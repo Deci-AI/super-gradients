3cfbbebf2bd71a67ffb58e5296e507ff162e8ed0
Louis Dupont <louis-dupont@live.fr>
2023-01-16 16:29:13 +0200

    wip

--- src/super_gradients/common/plugins/deci_platform_client.py
+++ src/super_gradients/common/plugins/deci_platform_client.py
@@ -13,11 +13,11 @@ def instantiate_deci_platform_client() -> DeciPlatformClient:
     :param api_port: The port of deci's platform HTTP API.
     :param https: Whether to use https instead of HTTP. Using https Will add latency.
     """
-    if client_enabled:
-        api_host = "api.deci.ai" if env_variables.PROD_ENVIRONMENT else "api.development.deci.ai"
-        print(api_host)
-        client = DeciPlatformClient(api_host=api_host)
-        client.login(token=env_variables.DECI_PLATFORM_TOKEN)
-        return client
-    else:
+    if not client_enabled:
         raise RuntimeError("Trying to instantiate deci_lab_client.client.DeciPlatformClient but deci_lab_client not installed...")
+
+    api_host = "api.deci.ai" if env_variables.PROD_ENVIRONMENT else "api.development.deci.ai"
+    print(api_host)
+    client = DeciPlatformClient(api_host=api_host)
+    client.login(token=env_variables.DECI_PLATFORM_TOKEN)
+    return client
