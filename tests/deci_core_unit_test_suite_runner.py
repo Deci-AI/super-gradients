38c9548c7352debcf350c99e3468ca666a13a540
Louis Dupont <louis-dupont@live.fr>
2023-01-17 16:21:26 +0200

    remove the test handler for simplicity and to follow the standard approach

--- tests/deci_core_unit_test_suite_runner.py
+++
@@ -1,8 +0,0 @@
-import sys
-import unittest
-
-
-if __name__ == "__main__":
-    unit_tests_suite = unittest.TestLoader().discover("./unit_tests/")
-    test_runner = unittest.TextTestRunner(verbosity=3, stream=sys.stdout)
-    test_runner.run(unit_tests_suite)
