f038425d004c2bb21855b6bf0e7a753794ff8650
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-01-23 15:34:13 +0200

    Added test for data loaders instantiation

--- tests/integration_tests/pose_estimation_dataset_test.py
+++ tests/integration_tests/pose_estimation_dataset_test.py
@@ -4,17 +4,12 @@ import unittest
 import pkg_resources
 from hydra import initialize_config_dir, compose
 from hydra.core.global_hydra import GlobalHydra
-
-import super_gradients
 from super_gradients.common.environment.path_utils import normalize_path
 from super_gradients.training.dataloaders.dataloaders import _process_dataset_params, get_data_loader
 from super_gradients.training.datasets.pose_estimation_datasets import COCOKeypointsDataset
 
 
 class PoseEstimationDatasetIntegrationTest(unittest.TestCase):
-    def setUp(self) -> None:
-        super_gradients.init_trainer()
-
     def test_datasets_instantiation(self):
         GlobalHydra.instance().clear()
         sg_recipes_dir = pkg_resources.resource_filename("super_gradients.recipes", "")
@@ -32,8 +27,8 @@ class PoseEstimationDatasetIntegrationTest(unittest.TestCase):
             assert val_dataset[0] is not None
 
     def test_dataloaders_instantiation(self):
-        train_loader = get_data_loader("coco_pose_estimation_dekr_dataset_params", COCOKeypointsDataset, train=True)
-        val_loader = get_data_loader("coco_pose_estimation_dekr_dataset_params", COCOKeypointsDataset, train=False)
+        train_loader = get_data_loader("coco_pose_estimation_dekr_dataset_params", COCOKeypointsDataset, train=True, dataloader_params=dict(num_workers=0))
+        val_loader = get_data_loader("coco_pose_estimation_dekr_dataset_params", COCOKeypointsDataset, train=False, dataloader_params=dict(num_workers=0))
 
         assert next(iter(train_loader)) is not None
         assert next(iter(val_loader)) is not None
