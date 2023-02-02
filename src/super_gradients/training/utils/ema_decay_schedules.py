cc3a6cdb0a36c08dc7e44ec7423e047581e32c6a
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-02-02 22:14:53 +0200

    Feature/sg 614 ema docs (#682)
    * EMA docs
    
    ---------
    
    Co-authored-by: Shay Aharon <80472096+shaydeci@users.noreply.github.com>

--- src/super_gradients/training/utils/ema_decay_schedules.py
+++ src/super_gradients/training/utils/ema_decay_schedules.py
@@ -1,5 +1,5 @@
-import math
 from abc import abstractmethod
+import numpy as np
 
 __all__ = ["IDecayFunction", "ConstantDecay", "ThresholdDecay", "ExpDecay", "EMA_DECAY_FUNCTIONS"]
 
@@ -44,7 +44,7 @@ class ThresholdDecay(IDecayFunction):
         pass
 
     def __call__(self, decay: float, step, total_steps: int) -> float:
-        return min(decay, (1 + step) / (10 + step))
+        return np.minimum(decay, (1 + step) / (10 + step))
 
 
 class ExpDecay(IDecayFunction):
@@ -58,7 +58,26 @@ class ExpDecay(IDecayFunction):
 
     def __call__(self, decay: float, step, total_steps: int) -> float:
         x = step / total_steps
-        return decay * (1 - math.exp(-x * self.beta))
+        return decay * (1 - np.exp(-x * self.beta))
 
 
 EMA_DECAY_FUNCTIONS = {"constant": ConstantDecay, "threshold": ThresholdDecay, "exp": ExpDecay}
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    total_steps = 6_00_000
+    step = np.arange(total_steps)
+    decay = 0.999
+
+    plt.figure()
+    plt.plot(step, ExpDecay(beta=15)(decay, step, total_steps), label="exp(beta=15)")
+    plt.plot(step, ThresholdDecay()(decay, step, total_steps), label="threshold")
+    plt.plot(step, [ConstantDecay()(decay, step, total_steps)] * total_steps, label="constant")
+    plt.xlabel("Training step")
+    plt.ylabel("Decay value")
+    plt.legend()
+    plt.title(f"EMA Decay Schedules (Max decay is {decay})")
+    plt.tight_layout()
+    plt.savefig("ema_decay_schedules.png")
+    plt.show()
