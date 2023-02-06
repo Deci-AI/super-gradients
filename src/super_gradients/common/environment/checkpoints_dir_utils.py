0affa5f7f979f9ae6d4ce0b960a3b6e0aad5bb3f
Louis Dupont <louis-dupont@live.fr>
2023-02-06 13:49:45 +0200

    wip

--- src/super_gradients/common/environment/checkpoints_dir_utils.py
+++ src/super_gradients/common/environment/checkpoints_dir_utils.py
@@ -25,7 +25,7 @@ def _parse_project_root_path(path: str) -> Optional[str]:
     """Extract the path of first project that includes this path (recursively look into parent folders). Return None if no project found."""
     if path in ("", "/"):
         return None
-    is_project_root_path = any(os.path.exists(os.path.join(path, file)) for file in (".git", "requirements.txt", ".env", "setup.py"))
+    is_project_root_path = any(os.path.exists(os.path.join(path, file)) for file in (".git", "requirements.txt", ".env", "venv", "setup.py"))
     return path if is_project_root_path else _parse_project_root_path(path=os.path.dirname(path))
 
 
