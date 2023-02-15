67634823a791abf4e49c98a2f7dc319e784d9dcd
NatanBagrov <natanzb@gmail.com>
2023-02-15 12:36:06 +0200

    fix

--- src/super_gradients/training/models/segmentation_models/segmentation_module.py
+++ src/super_gradients/training/models/segmentation_models/segmentation_module.py
@@ -60,11 +60,3 @@ class SegmentationModule(SgModule, ABC):
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-
-
-class CustomSegmentationModule(SegmentationModule):
-    def __init__(self, encoder_params, decoder_params, head_params, auxiliary_heads_params):
-        encoder_type = encoder_params.pop("type")
-        encoder = SEGMENTATION_ENCODERS[encoder_type](encoder_params)
-
-        super().__init__()
