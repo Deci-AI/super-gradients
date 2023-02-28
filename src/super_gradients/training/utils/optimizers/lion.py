d8e269a694ea006b279cd476622adc577c6494e8
shayaharon <shay.aharon@deci.ai>
2023-02-28 11:54:17 +0200

    cleanup

--- src/super_gradients/training/utils/optimizers/lion.py
+++ src/super_gradients/training/utils/optimizers/lion.py
@@ -1,24 +1,14 @@
-# Copyright 2023 Google Research. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""PyTorch implementation of the Lion optimizer."""
+"""PyTorch implementation of the Lion optimizer.
+Code adopted from: https://github.com/google/automl/blob/master/lion/lion_pytorch.py
+"""
 import torch
 from torch.optim.optimizer import Optimizer
 
 
 class Lion(Optimizer):
-    r"""Implements Lion algorithm."""
+    r"""Implements Lion algorithm.
+    Generaly, it is recommended to divide lr used by AdamW by 10 and multiply the weight decay by 10.
+    """
 
     def __init__(self, params, lr=1e-4, betas=(0.9, 0.99), weight_decay=0.0):
         """Initialize the hyperparameters.
