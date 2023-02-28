54fc608a40da1f4c64cb5ddfdbc190dd5a4bd482
Borys Tymchenko <tim4bor@gmail.com>
2023-02-28 15:20:54 +0200

    Added calib_dl, reset_calibrators and swith for augs

--- src/super_gradients/training/qat_trainer/qat_trainer.py
+++ src/super_gradients/training/qat_trainer/qat_trainer.py
@@ -2,6 +2,7 @@ import os
 from typing import Union, Tuple
 
 import hydra
+import torch.cuda
 from omegaconf import DictConfig
 from omegaconf import OmegaConf
 from torch import nn
@@ -89,6 +90,29 @@ class QATTrainer(Trainer):
             dataloader_params=cfg.dataset_params.val_dataloader_params.copy(),
         )
 
+        if "calib_dataloader" in cfg:
+            calib_dataloader_name = get_param(cfg, "calib_dataloader")
+            calib_dataloader_params = cfg.dataset_params.calib_dataloader_params.copy()
+            calib_dataset_params = cfg.dataset_params.calib_dataset_params.copy()
+        else:
+            calib_dataloader_name = get_param(cfg, "train_dataloader")
+            calib_dataloader_params = cfg.dataset_params.train_dataloader_params.copy()
+            calib_dataset_params = cfg.dataset_params.train_dataset_params.copy()
+
+            # if we use whole dataloader for calibration, don't shuffle it
+            # HistogramCalibrator collection routine is sensitive to order of batches and produces slightly different results
+            # if we use several batches, we don't want it to be from one class if it's sequential in dataloader
+            # model is in eval mode, so BNs will not be affected
+            calib_dataloader_params.shuffle = cfg.quantization_params.calib_params.num_calib_batches is not None
+            # we don't need training transforms during calibration, distribution of activations will be skewed
+            calib_dataset_params.transforms = cfg.dataset_params.val_dataset_params.transforms
+
+        calib_dataloader = dataloaders.get(
+            name=calib_dataloader_name,
+            dataset_params=calib_dataset_params,
+            dataloader_params=calib_dataloader_params,
+        )
+
         # BUILD MODEL
         model = models.get(
             model_name=cfg.arch_params.get("model_name", None) or cfg.architecture,
@@ -124,10 +148,11 @@ class QATTrainer(Trainer):
         calibrator.calibrate_model(
             model,
             method=cfg.quantization_params.calib_params.histogram_calib_method,
-            calib_data_loader=train_dataloader,
-            num_calib_batches=cfg.quantization_params.calib_params.num_calib_batches or (512 // cfg.dataset_params.train_dataloader_params.batch_size) or 1,
+            calib_data_loader=calib_dataloader,
+            num_calib_batches=cfg.quantization_params.calib_params.num_calib_batches or len(train_dataloader),
             percentile=get_param(cfg.quantization_params.calib_params, "percentile", 99.99),
         )
+        calibrator.reset_calibrators(model)  # release memory taken by calibrators
 
         # VALIDATE PTQ MODEL AND PRINT SUMMARY
         logger.info("Validating PTQ model...")
@@ -147,6 +172,8 @@ class QATTrainer(Trainer):
             model.train()
             recipe_logged_cfg = {"recipe_config": OmegaConf.to_container(cfg, resolve=True)}
             trainer = Trainer(experiment_name=cfg.experiment_name, ckpt_root_dir=get_param(cfg, "ckpt_root_dir", default_val=None))
+            torch.cuda.empty_cache()
+
             res = trainer.train(
                 model=model,
                 train_loader=train_dataloader,
@@ -169,6 +196,7 @@ class QATTrainer(Trainer):
             input_size=input_shape,
             train=False,
         )
+
         logger.info(f"Exported {suffix.upper()} ONNX to {qdq_onnx_path}")
 
         return model, res
