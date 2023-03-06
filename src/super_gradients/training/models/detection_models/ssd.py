b8460aa072c85097ad0756ea755391b1b6182ed6
Louis Dupont <louis-dupont@live.fr>
2023-03-06 13:43:00 +0200

    wip

--- src/super_gradients/training/models/detection_models/ssd.py
+++ src/super_gradients/training/models/detection_models/ssd.py
@@ -16,11 +16,29 @@ class SSDMobileNetV1(CustomizableDetector):
     def __init__(self, arch_params: Union[HpmStruct, DictConfig], in_channels: int = 3):
         merged_arch_params = HpmStruct(**copy.deepcopy(DEFAULT_SSD_MOBILENET_V1_ARCH_PARAMS))
         merged_arch_params.override(**arch_params.to_dict())
-        super().__init__(merged_arch_params, in_channels=in_channels)
+        super().__init__(
+            backbone=merged_arch_params.backbone,
+            neck=merged_arch_params.neck,
+            heads=merged_arch_params.heads,
+            num_classes=merged_arch_params.num_classes,
+            bn_eps=merged_arch_params.bn_eps,
+            bn_momentum=merged_arch_params.bn_momentum,
+            inplace_act=merged_arch_params.inplace_act,
+            in_channels=in_channels,
+        )
 
 
 class SSDLiteMobileNetV2(CustomizableDetector):
     def __init__(self, arch_params: Union[HpmStruct, DictConfig], in_channels: int = 3):
         merged_arch_params = HpmStruct(**copy.deepcopy(DEFAULT_SSD_LITE_MOBILENET_V2_ARCH_PARAMS))
         merged_arch_params.override(**arch_params.to_dict())
-        super().__init__(merged_arch_params, in_channels=in_channels)
+        super().__init__(
+            backbone=merged_arch_params.backbone,
+            neck=merged_arch_params.neck,
+            heads=merged_arch_params.heads,
+            num_classes=merged_arch_params.num_classes,
+            bn_eps=merged_arch_params.bn_eps,
+            bn_momentum=merged_arch_params.bn_momentum,
+            inplace_act=merged_arch_params.inplace_act,
+            in_channels=in_channels,
+        )
