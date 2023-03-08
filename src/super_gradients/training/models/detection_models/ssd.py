32325d2a55c7ffb76a6cb481e3b944bbce5818f5
Eugene <ekhvedchenya@gmail.com>
2023-03-08 17:12:33 +0200

    Remove globals

--- src/super_gradients/training/models/detection_models/ssd.py
+++ src/super_gradients/training/models/detection_models/ssd.py
@@ -8,12 +8,10 @@ from super_gradients.training.utils.utils import HpmStruct
 from super_gradients.training.models.detection_models.customizable_detector import CustomizableDetector
 
 
-DEFAULT_SSD_MOBILENET_V1_ARCH_PARAMS = get_arch_params("ssd_mobilenetv1_arch_params")
-DEFAULT_SSD_LITE_MOBILENET_V2_ARCH_PARAMS = get_arch_params("ssd_lite_mobilenetv2_arch_params")
-
-
 class SSDMobileNetV1(CustomizableDetector):
     def __init__(self, arch_params: Union[HpmStruct, DictConfig], in_channels: int = 3):
+        DEFAULT_SSD_MOBILENET_V1_ARCH_PARAMS = get_arch_params("ssd_mobilenetv1_arch_params")
+
         merged_arch_params = HpmStruct(**copy.deepcopy(DEFAULT_SSD_MOBILENET_V1_ARCH_PARAMS))
         merged_arch_params.override(**arch_params.to_dict())
         super().__init__(merged_arch_params, in_channels=in_channels)
@@ -21,6 +19,8 @@ class SSDMobileNetV1(CustomizableDetector):
 
 class SSDLiteMobileNetV2(CustomizableDetector):
     def __init__(self, arch_params: Union[HpmStruct, DictConfig], in_channels: int = 3):
+        DEFAULT_SSD_LITE_MOBILENET_V2_ARCH_PARAMS = get_arch_params("ssd_lite_mobilenetv2_arch_params")
+
         merged_arch_params = HpmStruct(**copy.deepcopy(DEFAULT_SSD_LITE_MOBILENET_V2_ARCH_PARAMS))
         merged_arch_params.override(**arch_params.to_dict())
         super().__init__(merged_arch_params, in_channels=in_channels)
