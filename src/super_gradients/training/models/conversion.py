5a35765c086c67e4472bf5e2a852ec90ffa8d2bb
Louis Dupont <louis-dupont@live.fr>
2023-03-13 09:14:05 +0200

    rollback minor change

--- src/super_gradients/training/models/conversion.py
+++ src/super_gradients/training/models/conversion.py
@@ -15,7 +15,6 @@ from super_gradients.common.environment.cfg_utils import load_experiment_cfg
 from super_gradients.training.utils.sg_trainer_utils import parse_args
 import os
 import pathlib
-
 from onnxsim import simplify
 import onnx
 
