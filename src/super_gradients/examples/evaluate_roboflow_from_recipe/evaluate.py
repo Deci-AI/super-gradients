0ba1095ea93efbbfafe025c5527bc2416aa369a0
Louis Dupont <louis-dupont@live.fr>
2023-03-13 18:27:29 +0200

    minor change

--- src/super_gradients/examples/evaluate_roboflow_from_recipe/evaluate.py
+++ src/super_gradients/examples/evaluate_roboflow_from_recipe/evaluate.py
@@ -8,6 +8,7 @@ from super_gradients.training.utils import get_param
 from super_gradients.training.utils.distributed_training_utils import (
     setup_device,
 )
+from super_gradients.training.datasets.detection_datasets.roboflow import get_datasets_metadata, get_dataset_num_classes
 
 
 @hydra.main(config_path=pkg_resources.resource_filename("super_gradients.recipes", ""), version_base="1.2")
@@ -20,15 +21,20 @@ def main(cfg: DictConfig) -> None:
 
     cfg.multi_gpu = "Off"
     cfg.num_gpus = 1
-    from super_gradients.training.datasets.detection_datasets.roboflow100 import ROBOFLOW_DATASETS_NAMES_WITH_CATEGORY
 
     original_experiment_name = cfg.experiment_name
-    for dataset in list(ROBOFLOW_DATASETS_NAMES_WITH_CATEGORY.keys())[12:]:
-        cfg.dataset_params.dataset_name = dataset
-        cfg.experiment_name = original_experiment_name + "_" + dataset
+
+    datasets_metadata = get_datasets_metadata()
+    for dataset_name in list(datasets_metadata.keys())[12:]:
+        cfg.dataset_params.dataset_name = dataset_name
+        cfg.experiment_name = original_experiment_name + "_" + dataset_name
+        cfg.arch_params.num_classes = get_dataset_num_classes(dataset_name)
+
         print(f"launching: {cfg.experiment_name}")
 
         Trainer.train_from_config(cfg)
+
+        # FIXME: doesnt work because .hydra not found
         Trainer.evaluate_checkpoint(experiment_name=cfg.experiment_name, ckpt_name="ckpt_best.pth")
         x, y = Trainer.evaluate_from_recipe(cfg)
 
