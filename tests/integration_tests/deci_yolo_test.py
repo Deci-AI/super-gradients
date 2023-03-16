c169c6e2b0049b1864df847acedda87cd6e71c18
shayaharon <shay.aharon@deci.ai>
2023-03-16 19:55:37 +0100

    working url for s variant

--- tests/integration_tests/deci_yolo_test.py
+++ tests/integration_tests/deci_yolo_test.py
@@ -1,11 +1,26 @@
 import unittest
 from super_gradients.training import models
+from super_gradients.training.dataloaders import coco2017_val_deci_yolo
+from super_gradients.training import Trainer
+
+# - _target_: super_gradients.training.metrics.DetectionMetrics
+# normalize_targets: True
+# post_prediction_callback:
+# _target_: super_gradients.training.models.detection_models.yolo_base.YoloPostPredictionCallback
+# iou: 0.65
+# conf: 0.01
+# num_cls: 80
+from super_gradients.training.metrics import DetectionMetrics
+from super_gradients.training.models import YoloPostPredictionCallback
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        model = models.get("deciyolo_s", num_classes=8)
-        print(model)
+        trainer = Trainer("test_deci_yolo_s")
+        model = models.get("deciyolo_s", num_classes=80, pretrained_weights="coco")
+        dl = coco2017_val_deci_yolo()
+        metrics = [DetectionMetrics(post_prediction_callback=YoloPostPredictionCallback(iou=0.65, conf=0.01), num_cls=80, normalize_targets=False)]
+        trainer.test(model=model, test_loader=dl, test_metrics_list=metrics)
 
 
 if __name__ == "__main__":
