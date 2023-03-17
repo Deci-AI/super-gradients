06f06c35f042989492dd55e140e9fd0695ca9755
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-03-17 10:31:05 +0200

    Change class c'tor

--- src/super_gradients/training/models/detection_models/deci_yolo/deci_yolo.py
+++ src/super_gradients/training/models/detection_models/deci_yolo/deci_yolo.py
@@ -1,9 +1,12 @@
 import copy
+from typing import Union
 
 import torch
+from omegaconf import DictConfig
 from super_gradients.common.environment.cfg_utils import load_arch_params
 from super_gradients.common.object_names import Models
 from super_gradients.common.registry import register_model
+from super_gradients.training.models.arch_params_factory import get_arch_params
 from super_gradients.training.models.detection_models.customizable_detector import CustomizableDetector
 from super_gradients.training.utils import HpmStruct
 import hydra
@@ -11,11 +14,17 @@ import hydra
 
 @register_model(Models.DECIYOLO_S)
 class DeciYolo_S(CustomizableDetector):
-    def __init__(self, num_classes: int):
-        arch_params = load_arch_params("deciyolo_s_arch_params")
-        arch_params = hydra.utils.instantiate(arch_params)
-        arch_params["heads"][list(arch_params["heads"].keys())[0]]["num_classes"] = num_classes
-        super(DeciYolo_S, self).__init__(HpmStruct(**copy.deepcopy(arch_params)))
+    def __init__(self, arch_params: Union[HpmStruct, DictConfig], in_channels: int = 3):
+        default_arch_params = get_arch_params("deciyolo_s_arch_params")
+        merged_arch_params = HpmStruct(**copy.deepcopy(default_arch_params))
+        merged_arch_params.override(**arch_params.to_dict())
+        super().__init__(merged_arch_params, in_channels=in_channels)
+
+    # def __init__(self, num_classes: int):
+    #     arch_params = load_arch_params("deciyolo_s_arch_params")
+    #     arch_params = hydra.utils.instantiate(arch_params)
+    #     arch_params["heads"][list(arch_params["heads"].keys())[0]]["num_classes"] = num_classes
+    #     super(DeciYolo_S, self).__init__(HpmStruct(**copy.deepcopy(arch_params)))
 
     @property
     def num_classes(self):
