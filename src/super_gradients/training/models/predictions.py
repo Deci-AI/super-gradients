35717803ed9636d6a04d886010f078f532783c1b
Louis Dupont <louis-dupont@live.fr>
2023-04-03 11:15:11 +0300

    simplify DetectionPrediction class

--- src/super_gradients/training/models/predictions.py
+++ src/super_gradients/training/models/predictions.py
@@ -15,61 +15,27 @@ class Prediction(ABC):
 
 @dataclass
 class DetectionPrediction(Prediction):
+    """Represents a detection prediction, with bboxes represented in xyxy format."""
 
-    _bboxes: np.ndarray
-    _bbox_format: str
-
+    bboxes_xyxy: np.ndarray
     confidence: np.ndarray
     labels: np.ndarray
-    image_shape: Tuple[int, int]
 
     def __init__(self, bboxes: np.ndarray, bbox_format: str, confidence: np.ndarray, labels: np.ndarray, image_shape: Tuple[int, int]):
         """
         :param bboxes:      BBoxes in the format specified by bbox_format
         :param bbox_format: BBoxes format that can be a string ("xyxy", "cxywh", ...)
         :param confidence:  Confidence scores for each bounding box
-        :param labels:      Labels for each bounding box
-        :param image_shape: Shape of the image the prediction is made on
+        :param labels:      Labels for each bounding box.
+        :param image_shape: Shape of the image the prediction is made on, (H, W). This is used to convert bboxes to xyxy format
         """
-        self._bboxes = bboxes
-        self._bbox_format = bbox_format
+        factory = BBoxFormatFactory()
+        self.bboxes_xyxy = convert_bboxes(
+            bboxes=bboxes,
+            image_shape=image_shape,
+            source_format=factory.get(bbox_format),
+            target_format=factory.get("xyxy"),
+            inplace=False,
+        )
         self.confidence = confidence
         self.labels = labels
-        self.image_shape = image_shape
-
-    @property
-    def bboxes_xyxy(self):
-        return self._get_bbox_as("xyxy")
-
-    @bboxes_xyxy.setter
-    def bboxes_xyxy(self, bboxes: np.ndarray):
-        self._set_bbox_from(bboxes=bboxes, input_bbox_format="xyxy")
-
-    @property
-    def bboxes_cxcywh(self):
-        return self._get_bbox_as("cxcywh")
-
-    @bboxes_cxcywh.setter
-    def bboxes_cxcywh(self, bboxes: np.ndarray):
-        self._set_bbox_from(bboxes=bboxes, input_bbox_format="cxcywh")
-
-    def _get_bbox_as(self, desired_bbox_format: str):
-        factory = BBoxFormatFactory()
-        return convert_bboxes(
-            bboxes=self._bboxes,
-            image_shape=self.image_shape,
-            source_format=factory.get(self._bbox_format),
-            target_format=factory.get(desired_bbox_format),
-            inplace=False,
-        )
-
-    def _set_bbox_from(self, bboxes: np.ndarray, input_bbox_format: str):
-        factory = BBoxFormatFactory()
-        self._bboxes = convert_bboxes(
-            bboxes=bboxes,
-            image_shape=self.image_shape,
-            source_format=factory.get(input_bbox_format),
-            target_format=factory.get(self._bbox_format),
-            inplace=False,
-        )
-        self._bbox_format = input_bbox_format
