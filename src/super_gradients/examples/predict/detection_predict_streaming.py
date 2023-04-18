34d882b2fea5a10309c8a308b8ee9eebf4bd2203
Louis Dupont <louis-dupont@live.fr>
2023-04-18 15:16:21 +0300

    add minor doc

--- src/super_gradients/examples/predict/detection_predict_streaming.py
+++ src/super_gradients/examples/predict/detection_predict_streaming.py
@@ -4,6 +4,8 @@ from super_gradients.training import models
 
 # Note that currently only YoloX and PPYoloE are supported.
 model = models.get(Models.YOLOX_N, pretrained_weights="coco")
+
+# We want to use cuda if available to speed up inference.
 model = model.to("cuda" if torch.cuda.is_available() else "cpu")
 
 model.predict_webcam()
--- src/super_gradients/examples/predict/detection_predict_video.py
+++ src/super_gradients/examples/predict/detection_predict_video.py
@@ -5,14 +5,17 @@ from super_gradients.training import models
 
 # Note that currently only YoloX and PPYoloE are supported.
 model = models.get(Models.YOLOX_N, pretrained_weights="coco")
+
+# We want to use cuda if available to speed up inference.
 model = model.to("cuda" if torch.cuda.is_available() else "cpu")
 
 video_path = "pose_elephant_flip.mp4"
 
+# Download the video to the local file system.
 with open(video_path, mode="wb") as f:
     response = requests.get("https://deci-pretrained-models.s3.amazonaws.com/sample_images/pose_elephant_flip.mp4")
     f.write(response.content)
 
 predictions = model.predict(video_path)
 predictions.show()
-predictions.save("test.mp4")
+predictions.save("output_path.mp4")
