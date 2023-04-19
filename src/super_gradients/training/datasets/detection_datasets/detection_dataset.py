4d06a209702b7b93b969bdffac5741eb8d6e86b7
Louis-Dupont <35190946+Louis-Dupont@users.noreply.github.com>
2023-04-19 13:13:25 +0300

    Feature/sg 814 support yoloformat loader (#847)
    * first draft
    
    * improve naming
    
    * fix name
    
    * remove comments
    
    * wip
    
    * add comment

--- src/super_gradients/training/datasets/detection_datasets/detection_dataset.py
+++ src/super_gradients/training/datasets/detection_datasets/detection_dataset.py
@@ -119,7 +119,7 @@ class DetectionDataset(Dataset):
 
         self.data_dir = data_dir
         if not Path(data_dir).exists():
-            raise FileNotFoundError(f"data_dir={data_dir} not found. Please make sure that data_dir points toward your dataset.")
+            raise RuntimeError(f"data_dir={data_dir} not found. Please make sure that data_dir points toward your dataset.")
 
         # Number of images that are available (regardless of ignored images)
         self.n_available_samples = self._setup_data_source()
