dfef60d776e9828598ae69248aa8d71fa46fd8ca
shayaharon <shay.aharon@deci.ai>
2023-04-24 12:14:35 +0300

    added more docs

--- src/super_gradients/common/environment/omegaconf_utils.py
+++ src/super_gradients/common/environment/omegaconf_utils.py
@@ -26,18 +26,25 @@ class RecipeShortcutsCallback(Callback):
 
     def on_run_start(self, config: DictConfig, **kwargs: Any) -> None:
         config.lr, config.training_hyperparams.initial_lr = self._override_with_shortcut(config.lr, config.training_hyperparams.initial_lr)
+
         config.batch_size, config.dataset_params.train_dataloader_params.batch_size = self._override_with_shortcut(
             config.batch_size, config.dataset_params.train_dataloader_params.batch_size
         )
+
         config.val_batch_size, config.dataset_params.val_dataloader_params.batch_size = self._override_with_shortcut(
             config.val_batch_size, config.dataset_params.val_dataloader_params.batch_size
         )
+
         config.resume, config.training_hyperparams.resume = self._override_with_shortcut(config.resume, config.training_hyperparams.resume)
+
         config.epochs, config.training_hyperparams.max_epochs = self._override_with_shortcut(config.epochs, config.training_hyperparams.max_epochs)
+
         config.ema, config.training_hyperparams.ema = self._override_with_shortcut(config.ema, config.training_hyperparams.ema)
+
         config.num_workers, config.dataset_params.train_dataloader_params.num_workers = self._override_with_shortcut(
             config.num_workers, config.dataset_params.train_dataloader_params.num_workers
         )
+
         config.num_workers, config.dataset_params.val_dataloader_params.num_workers = self._override_with_shortcut(
             config.num_workers, config.dataset_params.val_dataloader_params.num_workers
         )
