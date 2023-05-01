84c77998c56e4169cdca1d9aa9c6e1b273e8b2f0
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-05-01 17:29:09 +0300

    Rename integration test file for pose estimation

--- tests/integration_tests/pose_estimation_models_test.py
+++ tests/integration_tests/pose_estimation_models_test.py
@@ -1,16 +1,11 @@
-import os
 import unittest
 
-import pkg_resources
 import torch
-from hydra import initialize_config_dir, compose
-from hydra.core.global_hydra import GlobalHydra
 from tqdm import tqdm
 
-from super_gradients.common.environment.path_utils import normalize_path
-from super_gradients.training.dataloaders.dataloaders import _process_dataset_params, get_data_loader
-from super_gradients.training.datasets.pose_estimation_datasets import COCOKeypointsDataset
 from super_gradients.training import models
+from super_gradients.training.dataloaders.dataloaders import get_data_loader
+from super_gradients.training.datasets.pose_estimation_datasets import COCOKeypointsDataset
 from super_gradients.training.metrics import PoseEstimationMetrics
 from super_gradients.training.models.pose_estimation_models.dekr_hrnet import DEKRWrapper, DEKRHorisontalFlipWrapper
 from super_gradients.training.utils import DEKRPoseEstimationDecodeCallback
@@ -40,29 +35,6 @@ class PoseEstimationDatasetIntegrationTest(unittest.TestCase):
             16,
             15,
         ]
-
-    def test_datasets_instantiation(self):
-        GlobalHydra.instance().clear()
-        sg_recipes_dir = pkg_resources.resource_filename("super_gradients.recipes", "")
-        dataset_config = os.path.join("dataset_params", "coco_pose_estimation_dekr_dataset_params")
-        with initialize_config_dir(config_dir=normalize_path(sg_recipes_dir), version_base="1.2"):
-            # config is relative to a module
-            cfg = compose(config_name=normalize_path(dataset_config))
-            train_dataset_params = _process_dataset_params(cfg, dict(), True)
-            val_dataset_params = _process_dataset_params(cfg, dict(), True)
-
-            train_dataset = COCOKeypointsDataset(**train_dataset_params)
-            assert train_dataset[0] is not None
-
-            val_dataset = COCOKeypointsDataset(**val_dataset_params)
-            assert val_dataset[0] is not None
-
-    def test_dataloaders_instantiation(self):
-        train_loader = get_data_loader("coco_pose_estimation_dekr_dataset_params", COCOKeypointsDataset, train=True, dataloader_params=dict(num_workers=0))
-        val_loader = get_data_loader("coco_pose_estimation_dekr_dataset_params", COCOKeypointsDataset, train=False, dataloader_params=dict(num_workers=0))
-
-        assert next(iter(train_loader)) is not None
-        assert next(iter(val_loader)) is not None
 
     def test_dekr_model(self):
         val_loader = get_data_loader(
