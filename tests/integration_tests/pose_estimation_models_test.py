ed1be3e4328658d5e83147acaec9a221a633887f
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-05-02 15:36:51 +0300

    Rename PoseEstimationDatasetIntegrationTest to PoseEstimationModelsIntegrationTest

--- tests/integration_tests/pose_estimation_models_test.py
+++ tests/integration_tests/pose_estimation_models_test.py
@@ -12,7 +12,7 @@ from super_gradients.training.utils import DEKRPoseEstimationDecodeCallback
 from super_gradients.training.utils.pose_estimation import RescoringPoseEstimationDecodeCallback
 
 
-class PoseEstimationDatasetIntegrationTest(unittest.TestCase):
+class PoseEstimationModelsIntegrationTest(unittest.TestCase):
     def setUp(self):
         self.oks_sigmas = [0.026, 0.025, 0.025, 0.035, 0.035, 0.079, 0.079, 0.072, 0.072, 0.062, 0.062, 1.007, 1.007, 0.087, 0.087, 0.089, 0.089]
         self.flip_indexes_heatmap = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15, 17]
