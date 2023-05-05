38e94b94ed6755a9a12990025eea20595f8cf296
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-05-05 14:10:26 +0300

    Fix unit test

--- tests/unit_tests/yolo_nas_tests.py
+++ tests/unit_tests/yolo_nas_tests.py
@@ -14,7 +14,7 @@ class TestYOLONAS(unittest.TestCase):
         """
         Validate that we can create a YOLO-NAS model with custom in_channels.
         """
-        model = models.get(Models.YOLO_NAS_S, arch_params=dict(in_channels=2))
+        model = models.get(Models.YOLO_NAS_S, arch_params=dict(in_channels=2), num_classes=17)
         model(torch.rand(1, 2, 640, 640))
 
 
