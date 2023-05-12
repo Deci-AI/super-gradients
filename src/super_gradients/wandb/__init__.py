dbcadfe7a91860f15e236a607b15b7c82fce726c
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-05-12 18:52:16 +0530

    update: fix imports

--- src/super_gradients/wandb/__init__.py
+++ src/super_gradients/wandb/__init__.py
@@ -1,1 +1,1 @@
-from super_gradients.wandb import visualize_image_detection_prediction_on_wandb, log_detection_results_to_wandb
+from super_gradients.wandb.log_predictions import visualize_image_detection_prediction_on_wandb, log_detection_results_to_wandb
