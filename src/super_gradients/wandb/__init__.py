acdf3ef341ef7a8eedeef79f737a546f76396695
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-05-15 15:40:46 +0530

    fix: unused imports

--- src/super_gradients/wandb/__init__.py
+++ src/super_gradients/wandb/__init__.py
@@ -1,1 +1,4 @@
 from super_gradients.wandb.log_predictions import visualize_image_detection_prediction_on_wandb, log_detection_results_to_wandb
+
+
+__all__ = ["visualize_image_detection_prediction_on_wandb", "log_detection_results_to_wandb"]
