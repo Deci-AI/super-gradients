2fcffc6c64a6ba6bd936a61e14759ae7fb4dff2b
Avi Lumelsky <avi.lumelsky@deci.ai>
2023-05-22 13:24:54 +0300

    Remove input_shape and added input_size, added tests

--- tests/unit_tests/export_coreml_test.py
+++ tests/unit_tests/export_coreml_test.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import unittest
+from time import sleep
 
 from torchvision.transforms import Compose, Normalize, Resize
 
@@ -10,13 +11,19 @@ from super_gradients.training.transforms import Standardize
 
 
 class TestModelsCoreMLExport(unittest.TestCase):
-    def test_models_onnx_export_with_deprecated_input_shape(self):
+    def test_models_onnx_export_with_explicit_input_size(self):
         pretrained_model = models.get(Models.RESNET18, num_classes=1000, pretrained_weights="imagenet")
         preprocess = Compose([Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         with tempfile.TemporaryDirectory() as tmpdirname:
             out_path = os.path.join(tmpdirname, "resnet18.mlmodel")
-            models.convert_to_coreml(model=pretrained_model, out_path=out_path, input_shape=(3, 256, 256), pre_process=preprocess)
+            models.convert_to_coreml(model=pretrained_model, out_path=out_path, input_size=(3, 256, 256), pre_process=preprocess)
             self.assertTrue(os.path.isfile(out_path))
+
+    def test_models_onnx_export_without_explicit_input_size_raises_error(self):
+        pretrained_model = models.get(Models.RESNET18, num_classes=1000, pretrained_weights="imagenet")
+        preprocess = Compose([Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        with self.assertRaises(KeyError):
+            models.convert_to_coreml(model=pretrained_model, out_path="some-output-path.coreml", pre_process=preprocess)
 
     def test_models_coreml_export(self, **export_kwargs):
         pretrained_model = models.get(Models.YOLO_NAS_S, num_classes=1000, pretrained_weights="coco")
@@ -32,6 +39,9 @@ class TestModelsCoreMLExport(unittest.TestCase):
                 prep_model_for_conversion_kwargs=dict(input_size=(1, 3, 640, 640)),
                 **export_kwargs,
             )
+
+            # for some reason, CoreML save model can take time and it not instant. Resolving this race.
+            sleep(1)
             self.assertTrue(os.path.isfile(model_path))
             self.assertTrue(model_path.endswith(".mlprogram" if export_kwargs.get("export_as_ml_program") else ".mlmodel"))
 
