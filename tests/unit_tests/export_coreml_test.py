62cdc00eefd775ffd79b10bda54b33abee510f45
Avi Lumelsky <avi.lumelsky@deci.ai>
2023-05-22 15:24:50 +0300

    Formetted with black

--- tests/unit_tests/export_coreml_test.py
+++ tests/unit_tests/export_coreml_test.py
@@ -12,18 +12,15 @@ from super_gradients.training.transforms import Standardize
 class TestModelsCoreMLExport(unittest.TestCase):
     def test_models_onnx_export_with_explicit_input_size(self):
         pretrained_model = models.get(Models.RESNET18, num_classes=1000, pretrained_weights="imagenet")
-        preprocess = Compose(
-            [Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        preprocess = Compose([Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         with tempfile.TemporaryDirectory() as tmpdirname:
             out_path = os.path.join(tmpdirname, "resnet18.mlmodel")
-            models.convert_to_coreml(model=pretrained_model, out_path=out_path, input_size=(3, 256, 256),
-                                     pre_process=preprocess)
+            models.convert_to_coreml(model=pretrained_model, out_path=out_path, input_size=(3, 256, 256), pre_process=preprocess)
             self.assertTrue(os.path.isfile(out_path))
 
     def test_models_onnx_export_without_explicit_input_size_raises_error(self):
         pretrained_model = models.get(Models.RESNET18, num_classes=1000, pretrained_weights="imagenet")
-        preprocess = Compose(
-            [Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        preprocess = Compose([Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         with self.assertRaises(KeyError):
             models.convert_to_coreml(model=pretrained_model, out_path="some-output-path.coreml", pre_process=preprocess)
 
@@ -31,8 +28,7 @@ class TestModelsCoreMLExport(unittest.TestCase):
         pretrained_model = models.get(Models.YOLO_NAS_S, num_classes=1000, pretrained_weights="coco")
 
         # Just for the sake of testing, not really COCO preprocessing
-        preprocess = Compose(
-            [Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        preprocess = Compose([Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         with tempfile.TemporaryDirectory() as tmpdirname:
             out_path = os.path.join(tmpdirname, "yolo_nas_s")
             model_path = models.convert_to_coreml(
