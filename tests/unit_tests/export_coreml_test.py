3fc785ed183f39f374074dcf75818dea7c22b2c3
Avi Lumelsky <avi.lumelsky@deci.ai>
2023-05-22 13:29:06 +0300

    Added tests, CR fixes

--- tests/unit_tests/export_coreml_test.py
+++ tests/unit_tests/export_coreml_test.py
@@ -1,7 +1,6 @@
 import os
 import tempfile
 import unittest
-from time import sleep
 
 from torchvision.transforms import Compose, Normalize, Resize
 
@@ -13,15 +12,18 @@ from super_gradients.training.transforms import Standardize
 class TestModelsCoreMLExport(unittest.TestCase):
     def test_models_onnx_export_with_explicit_input_size(self):
         pretrained_model = models.get(Models.RESNET18, num_classes=1000, pretrained_weights="imagenet")
-        preprocess = Compose([Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        preprocess = Compose(
+            [Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         with tempfile.TemporaryDirectory() as tmpdirname:
             out_path = os.path.join(tmpdirname, "resnet18.mlmodel")
-            models.convert_to_coreml(model=pretrained_model, out_path=out_path, input_size=(3, 256, 256), pre_process=preprocess)
+            models.convert_to_coreml(model=pretrained_model, out_path=out_path, input_size=(3, 256, 256),
+                                     pre_process=preprocess)
             self.assertTrue(os.path.isfile(out_path))
 
     def test_models_onnx_export_without_explicit_input_size_raises_error(self):
         pretrained_model = models.get(Models.RESNET18, num_classes=1000, pretrained_weights="imagenet")
-        preprocess = Compose([Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        preprocess = Compose(
+            [Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         with self.assertRaises(KeyError):
             models.convert_to_coreml(model=pretrained_model, out_path="some-output-path.coreml", pre_process=preprocess)
 
@@ -29,7 +31,8 @@ class TestModelsCoreMLExport(unittest.TestCase):
         pretrained_model = models.get(Models.YOLO_NAS_S, num_classes=1000, pretrained_weights="coco")
 
         # Just for the sake of testing, not really COCO preprocessing
-        preprocess = Compose([Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        preprocess = Compose(
+            [Resize(224), Standardize(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         with tempfile.TemporaryDirectory() as tmpdirname:
             out_path = os.path.join(tmpdirname, "yolo_nas_s")
             model_path = models.convert_to_coreml(
@@ -40,10 +43,14 @@ class TestModelsCoreMLExport(unittest.TestCase):
                 **export_kwargs,
             )
 
-            # for some reason, CoreML save model can take time and it not instant. Resolving this race.
-            sleep(1)
-            self.assertTrue(os.path.isfile(model_path))
-            self.assertTrue(model_path.endswith(".mlprogram" if export_kwargs.get("export_as_ml_program") else ".mlmodel"))
+            if export_kwargs.get("export_as_ml_program"):
+                # Expecting a directory
+                self.assertTrue(os.path.exists(model_path))
+                self.assertTrue(model_path.endswith(".mlprogram"))
+            else:
+                # Expecting a single file
+                self.assertTrue(os.path.isfile(model_path))
+                self.assertTrue(model_path.endswith(".mlmodel"))
 
     def test_models_coreml_export_as_mlprogram(self):
         self.test_models_coreml_export(export_as_ml_program=True)
