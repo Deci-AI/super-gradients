cfdb94f1379d0bf14f01e723dd56521869ad22c8
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-05-25 18:53:09 +0530

    update: refactor artifact saving functionality

--- src/super_gradients/common/plugins/wandb/__init__.py
+++ src/super_gradients/common/plugins/wandb/__init__.py
@@ -1,4 +1,5 @@
-from super_gradients.wandb.log_predictions import visualize_image_detection_prediction_on_wandb, log_detection_results_to_wandb
+from super_gradients.common.plugins.wandb.artifacts import save_wandb_artifact
+from super_gradients.common.plugins.wandb.log_predictions import visualize_image_detection_prediction_on_wandb, log_detection_results_to_wandb
 
 
-__all__ = ["visualize_image_detection_prediction_on_wandb", "log_detection_results_to_wandb"]
+__all__ = ["save_wandb_artifact", "visualize_image_detection_prediction_on_wandb", "log_detection_results_to_wandb"]
