64ba9e76e8cf1a0d547f1c285dae4e992c15622e
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-05-25 19:04:41 +0530

    fix: imports

--- src/super_gradients/common/plugins/wandb/__init__.py
+++ src/super_gradients/common/plugins/wandb/__init__.py
@@ -1,4 +1,4 @@
-from super_gradients.common.plugins.wandb.log_predictions import visualize_image_detection_prediction_on_wandb, log_detection_results_to_wandb
+from super_gradients.common.plugins.wandb.log_predictions import log_detection_results_to_wandb
 
 
-__all__ = ["visualize_image_detection_prediction_on_wandb", "log_detection_results_to_wandb"]
+__all__ = ["log_detection_results_to_wandb"]
