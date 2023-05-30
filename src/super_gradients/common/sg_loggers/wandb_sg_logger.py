3015a8bbdcd8dca8e851be1375ac73944abe3aa1
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-05-31 01:06:19 +0530

    update: warning

--- src/super_gradients/common/sg_loggers/wandb_sg_logger.py
+++ src/super_gradients/common/sg_loggers/wandb_sg_logger.py
@@ -107,10 +107,10 @@ class WandBSGLogger(BaseSGLogger):
         if wandb.run is None:
             run = wandb.init(project=project_name, name=experiment_name, entity=entity, resume=resumed, id=wandb_id, **kwargs)
         else:
-            wandb.termwarn(
+            logger.warning(
                 "A Weights & Biases run was initialized before initializing `WandBSGLogger`. This means that `super-gradients` cannot control the run ID to which this session will be logged."
             )
-            wandb.termward(f"In order to resume this run please call `wand.init(id={wandb.run.id}, resume='must')` before reinitializing `WandBSGLogger`.")
+            logger.warning(f"In order to resume this run please call `wand.init(id={wandb.run.id}, resume='must')` before reinitializing `WandBSGLogger`.")
             run = wandb.run
 
         if save_code:
