75c8af6bc89cba1b92ec61b58d1a8dd84da0cdb2
roikoren755 <26850796+roikoren755@users.noreply.github.com>
2023-06-01 15:10:40 +0300

    Update deci-lab-client usage (#1047)
    * Update deci-lab-client usage
    
    * Update requirements.pro.txt
    
    * Get etag from client
    
    * Fix black
    
    * fix
    
    * Rename last places
    
    ---------
    
    Co-authored-by: Louis Dupont <louis-dupont@live.fr>

--- src/super_gradients/examples/deci_lab_export_example/deci_lab_export_example.py
+++ src/super_gradients/examples/deci_lab_export_example/deci_lab_export_example.py
@@ -11,21 +11,16 @@ from super_gradients import Trainer
 from super_gradients.training.dataloaders.dataloaders import classification_test_dataloader
 from super_gradients.training.metrics import Accuracy, Top5
 from super_gradients.training.utils.callbacks import DeciLabUploadCallback, ModelConversionCheckCallback
-from deci_lab_client.models import (
-    Metric,
-    QuantizationLevel,
-    ModelMetadata,
-    OptimizationRequestForm,
-    HardwareType,
-    FrameworkType,
-)
+from deci_platform_client.models import QuantizationLevel, HardwareType
 
 
 def main(architecture_name: str):
     # Empty on purpose so that it can be fit to the trainer use case
     checkpoint_dir = ""
 
-    os.environ["DECI_PLATFORM_TOKEN"] = "YOUR_API_TOKEN_HERE"  # You can also set your token as environment variable using the commandline or your IDE.
+    # You can also set your token as environment variable using the commandline or your IDE.
+    os.environ["DECI_CLIENT_ID"] = "YOUR_CLIENT_ID_HERE"
+    os.environ["DECI_CLIENT_SECRET"] = "YOUR_SECRET_KEY_HERE"
     trainer = Trainer(
         f"lab_optimization_{architecture_name}_example",
         model_checkpoints_location="local",
@@ -36,37 +31,23 @@ def main(architecture_name: str):
 
     # CREATE META-DATA, AND OPTIMIZATION REQUEST FORM FOR DECI PLATFORM POST TRAINING CALLBACK
     model_name = f"{architecture_name}_for_deci_lab_export_example"
-    model_meta_data = ModelMetadata(
-        name=model_name,
-        primary_batch_size=1,
-        architecture=architecture_name.title(),
-        framework=FrameworkType.PYTORCH,
-        dl_task="classification",
-        input_dimensions=(3, 320, 320),
-        primary_hardware=HardwareType.K80,
-        dataset_name="ImageNet",
-        description=f"{architecture_name} deci.ai Test",
-        tags=["imagenet", architecture_name],
-    )
-
-    optimization_request_form = OptimizationRequestForm(
-        target_hardware=HardwareType.T4,
-        target_batch_size=1,
-        target_metric=Metric.LATENCY,
-        optimize_model_size=True,
-        quantization_level=QuantizationLevel.FP16,
-        optimize_autonac=True,
-    )
 
     # IT IS ALSO RECOMMENDED TO USE A PRE TRAINING MODEL CONVERSION CHECK CALLBACK, SO THAT ANY CONVERSION
     # ERRORS WON'T APPEAR FOR THE FIRST TIME ONLY AT THE END OF TRAINING:
 
     phase_callbacks = [
-        ModelConversionCheckCallback(model_meta_data=model_meta_data, opset_version=11),
-        DeciLabUploadCallback(
-            model_meta_data=model_meta_data,
-            optimization_request_form=optimization_request_form,
+        ModelConversionCheckCallback(
+            model_name=model_name,
+            input_dimensions=(3, 320, 320),
+            primary_batch_size=1,
             opset_version=11,
+        ),
+        DeciLabUploadCallback(
+            model_name=model_name,
+            input_dimensions=(3, 320, 320),
+            target_hardware_types=[HardwareType.T4],
+            target_batch_size=1,
+            target_quantization_level=QuantizationLevel.FP16,
         ),
     ]
 
@@ -90,7 +71,12 @@ def main(architecture_name: str):
 
     # RUN TRAINING. ONCE ALL EPOCHS ARE DONE THE OPTIMIZED MODEL FILE WILL BE LOCATED IN THE EXPERIMENT'S
     # CHECKPOINT DIRECTORY
-    trainer.train(model=model, training_params=train_params, train_loader=classification_test_dataloader(), valid_loader=classification_test_dataloader())
+    trainer.train(
+        model=model,
+        training_params=train_params,
+        train_loader=classification_test_dataloader(),
+        valid_loader=classification_test_dataloader(),
+    )
 
 
 if __name__ == "__main__":
