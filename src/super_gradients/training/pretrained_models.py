8d8aafa992e0a0edcfdd445dbe8f1662af35f8dd
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-06-05 08:14:57 +0300

    Remove links to S & M models

--- src/super_gradients/training/pretrained_models.py
+++ src/super_gradients/training/pretrained_models.py
@@ -49,8 +49,6 @@ MODEL_URLS = {
     "yolo_nas_l_coco": "https://sghub.deci.ai/models/yolo_nas_l_coco.pth",
     "dekr_w32_no_dc_coco_pose": "https://sghub.deci.ai/models/coco2017_pose_dekr_w32_no_dc.pth",
     "pose_rescoring_coco_coco_pose": "https://sghub.deci.ai/models/pose_rescoring_coco_coco_pose.pth",
-    "yolo_nas_pose_s_coco": "https://sghub.deci.ai/models/yolo_nas_pose_s_coco.pth",
-    "yolo_nas_pose_m_coco": "https://sghub.deci.ai/models/yolo_nas_pose_m_coco.pth",
     "yolo_nas_pose_l_coco": "https://sghub.deci.ai/models/yolo_nas_pose_l_coco.pth",
 }
 
