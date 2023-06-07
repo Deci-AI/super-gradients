10b2dd84df46bcb98e87d4bd35756331b30fe931
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-06-07 11:13:20 +0300

    Remove temp files

--- dekr_predict.py
+++
@@ -1,8 +0,0 @@
-from super_gradients.common.object_names import Models
-from super_gradients.training import models
-from super_gradients.training.models import DEKRPoseEstimationModel
-
-dekr: DEKRPoseEstimationModel = models.get(Models.DEKR_W32_NO_DC, pretrained_weights="coco_pose")
-result = dekr.predict("team.png", conf=0.2)
-result.show(keypoint_radius=8, show_confidence=False, joint_thickness=4)
-result.save(".")
