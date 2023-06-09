ec53f5b69401542cd3da26ddaff4a939a218ab55
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-06-09 14:49:08 +0300

    Added predict

--- src/super_gradients/training/models/pose_estimation_models/yolo_nas_pose.py
+++ src/super_gradients/training/models/pose_estimation_models/yolo_nas_pose.py
@@ -1,19 +1,26 @@
 import collections
 import copy
-from typing import Union, Tuple
+from functools import lru_cache
+from typing import Union, Tuple, Optional, List
 
+import numpy as np
 import torch.nn.functional
 from omegaconf import DictConfig
 from torch import nn, Tensor
 
 from super_gradients.common.decorators.factory_decorator import resolve_param
 from super_gradients.common.factories.activations_type_factory import ActivationsTypeFactory
+from super_gradients.common.factories.processing_factory import ProcessingFactory
 from super_gradients.common.object_names import Models
 from super_gradients.common.registry import register_model, register_detection_module
 from super_gradients.modules import BaseDetectionModule, ConvBNAct
 from super_gradients.training.models.arch_params_factory import get_arch_params
 from super_gradients.training.models.detection_models.customizable_detector import CustomizableDetector
+from super_gradients.training.pipelines.pipelines import PoseEstimationPipeline
+from super_gradients.training.processing.processing import Processing
 from super_gradients.training.utils import HpmStruct, get_param, DEKRPoseEstimationDecodeCallback
+from super_gradients.training.utils.media.image import ImageSource
+from super_gradients.training.utils.predict import ImagesPoseEstimationPrediction
 
 
 @register_detection_module()
@@ -108,10 +115,97 @@ class YoloNASPose(CustomizableDetector):
         )
 
     @staticmethod
-    def get_post_prediction_callback(conf: float, iou: float) -> DEKRPoseEstimationDecodeCallback:
+    def get_post_prediction_callback(conf: float = 0.05):
         return DEKRPoseEstimationDecodeCallback(
-            output_stride=4, keypoint_threshold=conf, nms_threshold=iou, max_num_people=30, apply_sigmoid=True, nms_num_threshold=8
+            min_confidence=conf,
+            keypoint_threshold=0.05,
+            nms_threshold=0.05,
+            apply_sigmoid=True,
+            max_num_people=30,
+            nms_num_threshold=8,
+            output_stride=4,
         )
+
+    @resolve_param("image_processor", ProcessingFactory())
+    def set_dataset_processing_params(
+        self,
+        edge_links: Union[np.ndarray, List[Tuple[int, int]]],
+        edge_colors: Union[np.ndarray, List[Tuple[int, int, int]]],
+        keypoint_colors: Union[np.ndarray, List[Tuple[int, int, int]]],
+        image_processor: Optional[Processing] = None,
+        conf: Optional[float] = None,
+    ) -> None:
+        """Set the processing parameters for the dataset.
+
+        :param image_processor: (Optional) Image processing objects to reproduce the dataset preprocessing used for training.
+        :param conf:            (Optional) Below the confidence threshold, prediction are discarded
+        """
+        self._edge_links = edge_links or self._edge_links
+        self._edge_colors = edge_colors or self._edge_colors
+        self._keypoint_colors = keypoint_colors or self._keypoint_colors
+        self._image_processor = image_processor or self._image_processor
+        self._default_nms_conf = conf or self._default_nms_conf
+
+    @lru_cache(maxsize=1)
+    def _get_pipeline(self, conf: Optional[float] = None, fuse_model: bool = True) -> PoseEstimationPipeline:
+        """Instantiate the prediction pipeline of this model.
+
+        :param conf:    (Optional) Below the confidence threshold, prediction are discarded.
+                        If None, the default value associated to the training is used.
+        :param fuse_model: If True, create a copy of the model, and fuse some of its layers to increase performance. This increases memory usage.
+        """
+        if None in (self._edge_links, self._image_processor, self._default_nms_conf):
+            raise RuntimeError(
+                "You must set the dataset processing parameters before calling predict.\n" "Please call `model.set_dataset_processing_params(...)` first."
+            )
+
+        conf = conf or self._default_nms_conf
+
+        if len(self._keypoint_colors) != self.num_joints:
+            raise RuntimeError(
+                "The number of colors for the keypoints ({}) does not match the number of joints ({})".format(len(self._keypoint_colors), self.num_joints)
+            )
+        if len(self._edge_colors) != len(self._edge_links):
+            raise RuntimeError(
+                "The number of colors for the joints ({}) does not match the number of joint links ({})".format(len(self._edge_colors), len(self._edge_links))
+            )
+
+        pipeline = PoseEstimationPipeline(
+            model=self,
+            image_processor=self._image_processor,
+            edge_links=self._edge_links,
+            edge_colors=self._edge_colors,
+            keypoint_colors=self._keypoint_colors,
+            post_prediction_callback=self.get_post_prediction_callback(conf=conf),
+            fuse_model=fuse_model,
+        )
+        return pipeline
+
+    def predict(self, images: ImageSource, conf: Optional[float] = None, fuse_model: bool = True) -> ImagesPoseEstimationPrediction:
+        """Predict an image or a list of images.
+
+        :param images:  Images to predict.
+        :param conf:    (Optional) Below the confidence threshold, prediction are discarded.
+                        If None, the default value associated to the training is used.
+        :param fuse_model: If True, create a copy of the model, and fuse some of its layers to increase performance. This increases memory usage.
+        """
+        pipeline = self._get_pipeline(conf=conf, fuse_model=fuse_model)
+        return pipeline(images)  # type: ignore
+
+    def predict_webcam(self, conf: Optional[float] = None, fuse_model: bool = True):
+        """Predict using webcam.
+
+        :param conf:    (Optional) Below the confidence threshold, prediction are discarded.
+                        If None, the default value associated to the training is used.
+        :param fuse_model: If True, create a copy of the model, and fuse some of its layers to increase performance. This increases memory usage.
+        """
+        pipeline = self._get_pipeline(conf=conf, fuse_model=fuse_model)
+        pipeline.predict_webcam()
+
+    def train(self, mode: bool = True):
+        self._get_pipeline.cache_clear()
+        torch.cuda.empty_cache()
+        return super().train(mode)
 
     @property
     def num_classes(self):
