4c3e328ec48aad816bd14165f298b221491e1b8c
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-06-13 00:24:57 +0530

    update: WandBDetectionValidationPredictionLoggerCallback

--- src/super_gradients/common/plugins/wandb/validation_logger.py
+++ src/super_gradients/common/plugins/wandb/validation_logger.py
@@ -1,3 +1,4 @@
+from super_gradients.training.utils.callbacks.base_callbacks import PhaseContext
 import wandb
 import torch
 import numpy as np
@@ -37,9 +38,13 @@ class WandBDetectionValidationPredictionLoggerCallback(Callback):
             multi_label_per_box=multi_label_per_box,
         )
         self.wandb_images = []
+        self.epoch_count = 0
+        self.mean_prediction_dicts = []
+        self.wandb_table = wandb.Table(columns=["Epoch", "Prediction", "Mean-Confidence"])
 
     def on_validation_batch_end(self, context: PhaseContext) -> None:
         self.wandb_images = []
+        mean_prediction_dict = {class_name: 0.0 for class_name in self.class_names}
         post_nms_predictions = self.post_prediction_callback(context.preds, device=context.device)
         for prediction, image in zip(post_nms_predictions, context.inputs):
             prediction = prediction if prediction is not None else torch.zeros((0, 6), dtype=torch.float32)
@@ -56,9 +61,18 @@ class WandBDetectionValidationPredictionLoggerCallback(Callback):
                     image_shape=image.shape,
                 ),
             )
+            for predicted_label, prediction_confidence in zip(prediction[:, 5], prediction[:, 4]):
+                mean_prediction_dict[self.class_names[int(predicted_label)]] += prediction_confidence
+            mean_prediction_dict = {k: v / len(prediction[:, 4]) for k, v in mean_prediction_dict.items()}
+            self.mean_prediction_dicts.append(mean_prediction_dict)
             wandb_image = visualize_image_detection_prediction_on_wandb(prediction=image_prediction, show_confidence=True)
             self.wandb_images.append(wandb_image)
 
     def on_validation_loader_end(self, context: PhaseContext) -> None:
-        wandb.log({"Validation-Predictions": self.wandb_images})
-        self.wandb_images = []
+        for wandb_image, mean_prediction_dict in zip(self.wandb_images, self.mean_prediction_dicts):
+            self.wandb_table.add_data(self.epoch_count, wandb_image, mean_prediction_dict)
+        self.wandb_images, self.mean_prediction_dicts = [], []
+        self.epoch_count += 1
+    
+    def on_training_end(self, context: PhaseContext) -> None:
+        wandb.log({"Validation-Prediction": self.wandb_table})
