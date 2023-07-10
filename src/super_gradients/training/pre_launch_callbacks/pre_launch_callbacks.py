1da7133157518e637a69f16ea23fff6411edf0fe
Louis-Dupont <35190946+Louis-Dupont@users.noreply.github.com>
2023-07-10 13:10:36 +0300

    Adding test loaders (#1241)
    * first draft
    
    * improve text
    
    * fix
    
    * remove unwanted code
    
    * wip
    
    * wip
    
    * add example
    
    * remove unwanted change:
    
    * improve display
    
    * add test
    
    * remove unwanted change
    
    * add typing for mettrics
    
    * undo unwanted change
    
    * work with dict
    
    * add tests
    
    * Feature/sg 946 simpify torch schedulers integration (#1230)
    
    * test added
    
    * docs added and cleanup
    
    * docs finalized
    
    * added unit test to suite
    
    * added unit tests for all schedulers
    
    * added warning for update_param_groups
    
    * added clarification for phase arg
    
    * refactored unt test
    
    ---------
    
    Co-authored-by: Eugene Khvedchenya <ekhvedchenya@gmail.com>
    
    * add .
    
    * add run_test_freq
    
    * fix
    
    * add optional
    
    * fix
    
    ---------
    
    Co-authored-by: Eugene Khvedchenya <ekhvedchenya@gmail.com>
    Co-authored-by: Shay Aharon <80472096+shaydeci@users.noreply.github.com>

--- src/super_gradients/training/pre_launch_callbacks/pre_launch_callbacks.py
+++ src/super_gradients/training/pre_launch_callbacks/pre_launch_callbacks.py
@@ -109,6 +109,7 @@ class AutoTrainBatchSizeSelectionCallback(PreLaunchCallback):
         tmp_cfg.training_hyperparams.batch_accumulate = 1
         tmp_cfg.training_hyperparams.max_train_batches = self.num_forward_passes
         tmp_cfg.training_hyperparams.run_validation_freq = 2
+        tmp_cfg.training_hyperparams.run_test_freq = 2
         tmp_cfg.training_hyperparams.silent_mode = True
         tmp_cfg.training_hyperparams.save_model = False
         tmp_cfg.training_hyperparams.max_epochs = 1
