7f066769e6f3fbef1eb4015909c5e77ed2c7f27e
Louis Dupont <louis-dupont@live.fr>
2023-07-18 15:15:54 +0300

    set HYDRA_FULL_ERROR

--- src/super_gradients/common/crash_handler/crash_handler.py
+++ src/super_gradients/common/crash_handler/crash_handler.py
@@ -1,8 +1,8 @@
 import sys
-
 from super_gradients.common.crash_handler.crash_tips_setup import setup_crash_tips
 from super_gradients.common.crash_handler.exception_monitoring_setup import setup_pro_user_monitoring
 from super_gradients.common.crash_handler.exception import register_exceptions
+from super_gradients.common.environment.env_variables import env_variables
 
 
 def setup_crash_handler():
@@ -10,4 +10,9 @@ def setup_crash_handler():
     is_setup_crash_tips = setup_crash_tips()
     is_setup_pro_user_monitoring = setup_pro_user_monitoring()
     if is_setup_crash_tips or is_setup_pro_user_monitoring:  # We don't want to wrap sys.excepthook when not required
+
+        # This prevents hydra.main to catch errors that happen in the decorated function
+        # (which leads sys.excepthook to never be called)
+        env_variables.HYDRA_FULL_ERROR = "1"
+
         sys.excepthook = register_exceptions(sys.excepthook)
