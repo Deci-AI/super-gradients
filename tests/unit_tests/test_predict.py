e2638e0426bc017961b88af3246dcfcec007d5c7
Louis Dupont <louis-dupont@live.fr>
2023-07-24 13:36:01 +0300

    add test

---
+++ tests/unit_tests/test_predict.py
@@ -0,0 +1,29 @@
+import unittest
+import tempfile
+from super_gradients.common.object_names import Models
+from super_gradients.training import models
+
+
+class TrainWithTorchSchedulerTest(unittest.TestCase):
+    def setUp(self) -> None:
+        import os
+
+        rootdir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        self.images = [
+            os.path.join(rootdir, "documentation", "source", "images", "examples", "countryside.jpg"),
+            os.path.join(rootdir, "documentation", "source", "images", "examples", "street_busy.jpg"),
+            "https://cdn-attachments.timesofmalta.com/cc1eceadde40d2940bc5dd20692901371622153217-1301777007-4d978a6f-620x348.jpg",
+        ]
+
+    def test_classification_models(self):
+        model = models.get(Models.RESNET18, pretrained_weights="imagenet")
+
+        with tempfile.TemporaryDirectory() as tmp_dirname:
+            predictions = model.predict(self.images)
+            predictions.draw()
+            predictions.show()
+            predictions.save(output_folder=tmp_dirname)
+
+
+if __name__ == "__main__":
+    unittest.main()
