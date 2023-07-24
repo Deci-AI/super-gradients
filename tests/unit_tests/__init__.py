c653f3d087baf03d895ede0c29e723efc967cd20
Louis Dupont <louis-dupont@live.fr>
2023-07-24 14:01:18 +0300

    fix

--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -24,7 +24,7 @@ from tests.unit_tests.training_params_factory_test import TrainingParamsTest
 from tests.unit_tests.config_inspector_test import ConfigInspectTest
 from tests.unit_tests.transforms_test import TestTransforms
 from tests.unit_tests.post_prediction_callback_test import TestPostPredictionCallback
-from test_predict import TestModelPredict
+from tests.unit_tests.test_predict import TestModelPredict
 
 __all__ = [
     "CrashTipTest",
