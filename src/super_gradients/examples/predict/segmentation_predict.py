da34be57fa6736e36674543a38e6dd7ac767d06e
dafna-r <47064033+dafna-r@users.noreply.github.com>
2023-07-25 17:31:11 +0300

    adding segmentation predict

---
+++ src/super_gradients/examples/predict/segmentation_predict.py
@@ -0,0 +1,16 @@
+from super_gradients.common.object_names import Models
+from super_gradients.training import models
+
+# Note that currently only XXX, YYY and ZZZ are supported.
+# model = models.get(Models.PP_LITE_B_SEG, pretrained_weights="cityscapes")
+model = models.get(model_name=Models.PP_LITE_T_SEG75, arch_params={"use_aux_heads": False}, num_classes=19, pretrained_weights="cityscapes")
+
+IMAGES = [
+    "../../../../documentation/source/images/examples/countryside.jpg",
+    "../../../../documentation/source/images/examples/street_busy.jpg",
+    "https://cdn-attachments.timesofmalta.com/cc1eceadde40d2940bc5dd20692901371622153217-1301777007-4d978a6f-620x348.jpg",
+]
+
+predictions = model.predict(IMAGES)
+predictions.show()
+predictions.save(output_folder="")  # Save in working directory
