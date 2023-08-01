8ff0b9bafbb2dea58729ec992213b49b997ac322
Yael-Baron <137149571+Yael-Baron@users.noreply.github.com>
2023-08-01 12:30:03 +0300

    Added unit test for segformer

--- tests/unit_tests/pretrained_models_unit_test.py
+++ tests/unit_tests/pretrained_models_unit_test.py
@@ -3,8 +3,8 @@ import super_gradients
 from super_gradients.common.object_names import Models
 from super_gradients.training import models
 from super_gradients.training import Trainer
-from super_gradients.training.dataloaders.dataloaders import classification_test_dataloader
-from super_gradients.training.metrics import Accuracy
+from super_gradients.training.dataloaders.dataloaders import classification_test_dataloader, segmentation_test_dataloader
+from super_gradients.training.metrics import Accuracy, IoU
 import os
 import shutil
 
@@ -28,6 +28,36 @@ class PretrainedModelsUnitTest(unittest.TestCase):
         trainer = Trainer("imagenet_pretrained_repvgg_a0_unit_test")
         model = models.get(Models.REPVGG_A0, pretrained_weights="imagenet", arch_params={"build_residual_branches": True})
         trainer.test(model=model, test_loader=classification_test_dataloader(), test_metrics_list=[Accuracy()], metrics_progress_verbose=True)
+
+    def test_pretrained_segformer_b0_cityscapes(self):
+        trainer = Trainer("cityscapes_pretrained_segformer_b0_unit_test")
+        model = models.get(Models.SEGFORMER_B0, pretrained_weights="cityscapes")
+        trainer.test(model=model, test_loader=segmentation_test_dataloader(), test_metrics_list=[IoU(num_classes=20)], metrics_progress_verbose=True)
+
+    def test_pretrained_segformer_b1_cityscapes(self):
+        trainer = Trainer("cityscapes_pretrained_segformer_b1_unit_test")
+        model = models.get(Models.SEGFORMER_B1, pretrained_weights="cityscapes")
+        trainer.test(model=model, test_loader=segmentation_test_dataloader(), test_metrics_list=[IoU(num_classes=20)], metrics_progress_verbose=True)
+
+    def test_pretrained_segformer_b2_cityscapes(self):
+        trainer = Trainer("cityscapes_pretrained_segformer_b2_unit_test")
+        model = models.get(Models.SEGFORMER_B2, pretrained_weights="cityscapes")
+        trainer.test(model=model, test_loader=segmentation_test_dataloader(), test_metrics_list=[IoU(num_classes=20)], metrics_progress_verbose=True)
+
+    def test_pretrained_segformer_b3_cityscapes(self):
+        trainer = Trainer("cityscapes_pretrained_segformer_b3_unit_test")
+        model = models.get(Models.SEGFORMER_B3, pretrained_weights="cityscapes")
+        trainer.test(model=model, test_loader=segmentation_test_dataloader(), test_metrics_list=[IoU(num_classes=20)], metrics_progress_verbose=True)
+
+    def test_pretrained_segformer_b4_cityscapes(self):
+        trainer = Trainer("cityscapes_pretrained_segformer_b4_unit_test")
+        model = models.get(Models.SEGFORMER_B4, pretrained_weights="cityscapes")
+        trainer.test(model=model, test_loader=segmentation_test_dataloader(), test_metrics_list=[IoU(num_classes=20)], metrics_progress_verbose=True)
+
+    def test_pretrained_segformer_b5_cityscapes(self):
+        trainer = Trainer("cityscapes_pretrained_segformer_b5_unit_test")
+        model = models.get(Models.SEGFORMER_B5, pretrained_weights="cityscapes")
+        trainer.test(model=model, test_loader=segmentation_test_dataloader(), test_metrics_list=[IoU(num_classes=20)], metrics_progress_verbose=True)
 
     def tearDown(self) -> None:
         if os.path.exists("~/.cache/torch/hub/"):
