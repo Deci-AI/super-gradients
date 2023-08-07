e6baa8af0e7a13b3784a1593cf302d223a9ede07
Shay Aharon <80472096+shaydeci@users.noreply.github.com>
2023-08-07 13:40:23 +0300

    Feature/sg 901 extreme batch visualization for object detection (#1339)
    * added docs, added option for empty targets in visualization
    
    * added unit tests
    
    * added typing, classes message and recipe cleanup

--- src/super_gradients/training/utils/detection_utils.py
+++ src/super_gradients/training/utils/detection_utils.py
@@ -501,7 +501,7 @@ class DetectionVisualization:
         :param image_tensor:            rgb images, (B, H, W, 3)
         :param pred_boxes:              boxes after NMS for each image in a batch, each (Num_boxes, 6),
                                         values on dim 1 are: x1, y1, x2, y2, confidence, class
-        :param target_boxes:            (Num_targets, 6), values on dim 1 are: image id in a batch, class, x y w h
+        :param target_boxes:            (Num_targets, 6), values on dim 1 are: image id in a batch, class, cx cy w h
                                         (coordinates scaled to [0, 1])
         :param batch_name:              id of the current batch to use for image naming
 
@@ -518,6 +518,8 @@ class DetectionVisualization:
         """
         image_np = undo_preprocessing_func(image_tensor.detach())
         targets = DetectionVisualization._scaled_ccwh_to_xyxy(target_boxes.detach().cpu().numpy(), *image_np.shape[1:3], image_scale)
+        if pred_boxes is None:
+            pred_boxes = [None for _ in range(image_np.shape[0])]
 
         out_images = []
         for i in range(image_np.shape[0]):
