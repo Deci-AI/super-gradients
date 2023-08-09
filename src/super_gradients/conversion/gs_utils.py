cd278aae4498cb44763738b81b9ba8446ff50cf7
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-08-09 11:57:38 +0200

    Do imports of gs inside the export call

--- src/super_gradients/conversion/gs_utils.py
+++ src/super_gradients/conversion/gs_utils.py
@@ -7,3 +7,16 @@ def import_onnx_graphsurgeon_or_fail_with_instructions():
             "Please install it with pip install onnx_graphsurgeon>=0.3.8,<0.4 --extra-index-url https://pypi.ngc.nvidia.com"
         )
     return gs
+
+
+def import_onnx_graphsurgeon_or_install():
+    try:
+        import onnx_graphsurgeon as gs
+
+        return gs
+    except ImportError:
+        import pip
+
+        pip.main(["install", "onnx_graphsurgeon>=0.3.8,<0.4", "--extra-index-url", "https://pypi.ngc.nvidia.com"])
+
+        return import_onnx_graphsurgeon_or_fail_with_instructions()
