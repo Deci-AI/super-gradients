4ec8187f3b19bb9da202c961f5ed2401327f7505
shayaharon <shay.aharon@deci.ai>
2023-08-13 15:53:53 +0300

    renamed vars in example

--- src/super_gradients/examples/predict/detection_predict_with_labels.py
+++ src/super_gradients/examples/predict/detection_predict_with_labels.py
@@ -13,13 +13,15 @@ dataset = COCODetectionDataset(
 
 # x's are np.ndarrays images of shape (H,W,3)
 # y's are np.ndarrays of shape (num_boxes,x1,y1,x2,y2,class_id)
-x1, y1, _ = dataset[0]
-x2, y2, _ = dataset[1]
+image1, target1, _ = dataset[0]
+image2, target2, _ = dataset[1]
 
 # images from COCODetectionDataset are RGB and images as np.ndarrays are expected to be BGR
-x2 = x2[:, :, ::-1]
-x1 = x1[:, :, ::-1]
+image2 = image2[:, :, ::-1]
+image1 = image1[:, :, ::-1]
 
-predictions = model.predict([x1, x2], target_bboxes=[y1[:, :4], y2[:, :4]], target_class_ids=[y1[:, 4], y2[:, 4]], target_bboxes_format="xyxy")
+predictions = model.predict(
+    [image1, image2], target_bboxes=[target1[:, :4], target2[:, :4]], target_class_ids=[target1[:, 4], target2[:, 4]], target_bboxes_format="xyxy"
+)
 predictions.show()
 predictions.save(output_folder="")  # Save in working directory
