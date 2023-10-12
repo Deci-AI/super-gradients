b25434de92b0539771d17464fac34cade59337df
shayaharon <shay.aharon@deci.ai>
2023-10-12 11:52:52 +0300

    fixed last unit test

--- tests/unit_tests/training_params_factory_test.py
+++ tests/unit_tests/training_params_factory_test.py
@@ -10,7 +10,7 @@ class TrainingParamsTest(unittest.TestCase):
 
     def test_get_train_params_with_overrides(self):
         train_params = training_hyperparams.coco2017_yolox_train_params(overriding_params={"max_epochs": 5})
-        self.assertTrue(train_params["loss"] == "YoloXDetectionLoss")
+        self.assertTrue(list(train_params["loss"].keys())[0] == "YoloXDetectionLoss")
         self.assertTrue(train_params["max_epochs"] == 5)
 
 
