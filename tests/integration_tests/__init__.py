91c9e2b923658bffe5f0557e2a101e1ff6a0fb29
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-10-13 17:15:56 +0300

    Added integration tests for YoloNASPose

--- tests/integration_tests/__init__.py
+++ tests/integration_tests/__init__.py
@@ -4,5 +4,6 @@ from tests.integration_tests.ema_train_integration_test import EMAIntegrationTes
 from tests.integration_tests.lr_test import LRTest
 from tests.integration_tests.pose_estimation_models_test import PoseEstimationModelsIntegrationTest
 from tests.integration_tests.yolo_nas_integration_test import YoloNASIntegrationTest
+from tests.integration_tests.yolo_nas_pose_integration_test import YoloNASPoseIntegrationTest
 
-__all__ = ["EMAIntegrationTest", "LRTest", "PoseEstimationModelsIntegrationTest", "YoloNASIntegrationTest"]
+__all__ = ["EMAIntegrationTest", "LRTest", "PoseEstimationModelsIntegrationTest", "YoloNASIntegrationTest", "YoloNASPoseIntegrationTest"]
