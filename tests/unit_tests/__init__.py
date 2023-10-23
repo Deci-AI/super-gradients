f6633000a214fc74c8ce314ac4fd8177e4e4f10e
shayaharon <shay.aharon@deci.ai>
2023-10-23 13:20:56 +0300

    added tests to suite

--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -5,6 +5,7 @@ from tests.unit_tests.factories_test import FactoriesTest
 from tests.unit_tests.optimizer_params_override_test import TrainOptimizerParamsOverride
 from tests.unit_tests.resume_training_test import ResumeTrainingTest
 from tests.unit_tests.strictload_enum_test import StrictLoadEnumTest
+from tests.unit_tests.test_export_recipe import TestExportRecipe
 from tests.unit_tests.train_after_test_test import CallTrainAfterTestTest
 from tests.unit_tests.zero_weight_decay_on_bias_bn_test import ZeroWdForBnBiasTest
 from tests.unit_tests.save_ckpt_test import SaveCkptListUnitTest
@@ -55,4 +56,5 @@ __all__ = [
     "TestPostPredictionCallback",
     "TestModelPredict",
     "TestDeprecationDecorator",
+    "TestExportRecipe",
 ]
