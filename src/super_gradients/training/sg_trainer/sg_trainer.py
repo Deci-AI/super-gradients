0a381185d2335b97c0bd0f800032840d4467ca61
Louis Dupont <louis-dupont@live.fr>
2023-10-26 09:15:43 +0200

    add to

--- src/super_gradients/training/sg_trainer/sg_trainer.py
+++ src/super_gradients/training/sg_trainer/sg_trainer.py
@@ -384,6 +384,7 @@ class Trainer:
             checkpoint_path=cfg.checkpoint_params.checkpoint_path,
             load_backbone=cfg.checkpoint_params.load_backbone,
             checkpoint_num_classes=get_param(cfg.checkpoint_params, "checkpoint_num_classes"),
+            num_input_channels=get_param(cfg.arch_params, "num_input_channels"),
         )
 
         # TEST
