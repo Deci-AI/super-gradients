d11b121540e114ce251b5687f79bc1a5e292a2ac
hakuryuu96 <marchenkophilip@gmail.com>
2023-11-13 14:53:02 +0000

    Removed unused import

--- src/super_gradients/examples/predict/detection_predict_video.py
+++ src/super_gradients/examples/predict/detection_predict_video.py
@@ -2,8 +2,6 @@ import requests
 import torch
 from super_gradients.common.object_names import Models
 from super_gradients.training import models
-import numpy as np
-
 
 # Note that currently only YoloX, PPYoloE and YOLO-NAS are supported.
 model = models.get(Models.YOLO_NAS_L, pretrained_weights="coco")
