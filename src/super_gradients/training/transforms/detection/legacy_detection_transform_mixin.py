2c7979a7d3bdc8e81fbef24b20d3dd4e39af82dd
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2023-11-25 09:43:30 +0200

    Added docs

--- src/super_gradients/training/transforms/detection/legacy_detection_transform_mixin.py
+++ src/super_gradients/training/transforms/detection/legacy_detection_transform_mixin.py
@@ -30,6 +30,15 @@ class LegacyDetectionTransformMixin:
 
     @classmethod
     def convert_input_dict_to_detection_sample(cls, sample_annotations: Dict[str, Union[np.ndarray, Any]]) -> DetectionSample:
+        """
+        Convert old-style detection sample dict to new DetectionSample dataclass.
+
+        :param sample_annotations: Input dictionary with following keys:
+                                    - image: numpy array of [H,W,C] or [C,H,W] format
+                                    - target: numpy array of [N,5] shape with bounding box of each instance (XYXY + LABEL)
+                                    - crowd_targets: numpy array of [N,5] shape with bounding box of each instance (XYXY + LABEL)
+        :return: An instance of DetectionSample dataclass filled with data from input dictionary.
+        """
         target = sample_annotations["target"]
         if len(target) == 0:
             target = np.zeros((0, 5), dtype=np.float32)
@@ -58,7 +67,18 @@ class LegacyDetectionTransformMixin:
         )
 
     @classmethod
-    def convert_detection_sample_to_dict(cls, detection_sample: DetectionSample, include_crowd_target: bool) -> Dict[str, Union[np.ndarray, Any]]:
+    def convert_detection_sample_to_dict(cls, detection_sample: DetectionSample, include_crowd_target: Union[bool, None]) -> Dict[str, Union[np.ndarray, Any]]:
+        """
+        Convert new DetectionSample dataclass to old-style detection sample dict.
+        This is a reverse operation to convert_input_dict_to_detection_sample and used to make legacy transforms compatible with new detection transforms.
+        :param detection_sample:     Input DetectionSample dataclass.
+        :param include_crowd_target: A flag indicating whether to include crowd_target in output dictionary.
+                                     Can be None - in this case crowd_target will be included only if crowd targets are present in input sample.
+        :return:                     Output dictionary with following keys:
+                                        - image: numpy array of [H,W,C] or [C,H,W] format
+                                        - target: numpy array of [N,5] shape with bounding box of each instance (XYXY + LABEL)
+                                        - crowd_targets: numpy array of [N,5] shape with bounding box of each instance (XYXY + LABEL)
+        """
         image = detection_sample.image
         crowd_mask = detection_sample.is_crowd > 0
         crowd_labels = detection_sample.labels[crowd_mask]
@@ -73,6 +93,8 @@ class LegacyDetectionTransformMixin:
             "image": image,
             "target": target,
         }
+        if include_crowd_target is None:
+            include_crowd_target = crowd_mask.any()
         if include_crowd_target:
             sample["crowd_target"] = crowd_target
         return sample
