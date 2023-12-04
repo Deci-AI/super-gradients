e5290cfaead1e817b15c34ba93ff11c0c88c8a1e
ofri masad <ofri@deci.ai>
2023-12-04 16:02:40 +0200

    wip

---
+++ docs/javascript/init_kapa_widget.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+ var script = document.createElement("script");
+ script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+ script.setAttribute("data-website-id", "XXX-XXX-XXX");
+ script.setAttribute("data-project-name", "super-gradients");
+ script.setAttribute("data-project-color", "#555555");
+ script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/Deci-AI/super-gradients/master/documentation/assets/SG_img/SG%20-%20Symbol.png");
+ script.async = true;
+ document.head.appendChild(script);
+});
