ba89f32461a1e8de7a05543014b7c2bd848c9dfb
shayaharon <shay.aharon@deci.ai>
2023-12-18 19:29:36 +0200

    fixed unit test

--- tests/unit_tests/depth_estimation_dataset_test.py
+++ tests/unit_tests/depth_estimation_dataset_test.py
@@ -2,8 +2,9 @@ import unittest
 from pathlib import Path
 import os
 
+import numpy as np
+
 from super_gradients.training.datasets.depth_estimation_datasets import NYUv2DepthEstimationDataset
-from super_gradients.training.samples import DepthEstimationSample
 
 
 class DepthEstimationDatasetTest(unittest.TestCase):
@@ -13,8 +14,9 @@ class DepthEstimationDatasetTest(unittest.TestCase):
 
     def test_normal_nyuv2_creation(self):
         dataset = NYUv2DepthEstimationDataset(root=self.mini_nyuv2_data_dir, df_path=self.mini_nyuv2_df_path)
-        for i, (sample) in enumerate(dataset):
-            self.assertTrue(isinstance(sample, DepthEstimationSample))
+        for i, (image, depth_map) in enumerate(dataset):
+            self.assertTrue(isinstance(image, np.ndarray))
+            self.assertTrue(isinstance(depth_map, np.ndarray))
         self.assertTrue(len(dataset) == 10 and i == 9)
 
     def test_depth_estimation_plot(self):
