5d1f689b51a17f105934cf3b1a6d2f47e4d11334
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2024-01-09 16:28:25 +0200

    Improve YoloNAS model flexibility (#1749)
    * Added random_aspect_ratio to PPYoloECollateFN
    
    * Check whether additional samples is in dict
    
    * Fix bug that class_names was ignored in DetectionMetrics when include_classwise_ap as enabled
    
    * DetectionAutoPadding._get_padding_params is not classmethod
    
    * Allow YoloNASStem to have stride argument
    
    * Allow use of multi-scale training
    
    * Allow use of multi-scale training
    
    * Allow use of multi-scale training
    
    * Add no grad & cast to float to support predictions in FP16 on CPU
    
    * Added logging
    
    * Fix warning
    
    * Fix warning
    
    * Fix warning
    
    * Fix warning
    
    * Fix warning
    
    * Fix warning
    
    * Fix warning
    
    * Fix warning
    
    * Fix warning
    
    * Fix visualization
    
    * Fix visualization
    
    * Allow context_module to be None
    
    * YoloNAS to return same outputs for train/validation (This is for visualization)
    
    * Register YoloNAS model
    
    * Allow specification of the stride
    
    * Optimize gather
    
    * make_upsample_module_v2
    
    * Ensure class names is list
    
    * Remove logging
    
    * Allow setting upsample
    
    * Remove _convert_
    
    * Fixed running test
    
    * UpsampleMode subclass from str to make it string enum
    
    * Added option to set IoU loss flavor
    
    * Implement wandb.flush and log epochs from zero
    
    * Remove flush
    
    * Added ability to do perfect classification
    
    * Added ability to do perfect classification
    
    * Added explicit conversion to tensor
    
    * Added support for class agnostic nms
    
    * Fix exception hint
    
    * Fix logic of saving extreme batch as epoch number is 0-based
    
    * Fix exception hint
    
    * Revert metric
    
    * Allow crowd detection collate fn take same args as non-crowd version
    
    * Bugfixes in distance-based metric
    
    * Allow multiple detection metrics to exist simultaneously
    
    * Fix typo
    
    * Multiple improvemeents
    
    * Allow post-prediction callback to work with traced model
    
    * PPYoloE loss does not require reg_max anymore
    
    * PPYoloE loss does not require reg_max anymore
    
    * PPYoloE loss does not require reg_max anymore
    
    * Fix callback
    
    * PPYoloE loss does not require reg_max anymore
    
    * Revert old behavior of epoch logging
    
    * Revert old behavior of epoch logging
    
    * Revert old behavior of epoch logging
    
    * Added num_classes
    
    * Add default params
    
    * Fixed bug in DetectionMixup when it's disabled
    
    * Revert changes in pose loss
    
    * Put back master version after wrong merge
    
    * Revert config change
    
    * make_upsample_module_with_explicit_channels
    
    * Simplify code

--- src/super_gradients/common/data_types/enum/upsample_mode.py
+++ src/super_gradients/common/data_types/enum/upsample_mode.py
@@ -1,9 +1,10 @@
 from enum import Enum
 
 
-class UpsampleMode(Enum):
+class UpsampleMode(str, Enum):
     NEAREST = "nearest"
     BILINEAR = "bilinear"
     BICUBIC = "bicubic"
     NN_PIXEL_SHUFFLE = "nn_pixel_shuffle"
     PIXEL_SHUFFLE = "pixel_shuffle"
+    CONV_TRANSPOSE = "conv_transpose"
