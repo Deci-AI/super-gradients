60d664c85ac71e054754e04f04905adc2c735dfa
shayaharon <shay.aharon@deci.ai>
2024-01-22 15:35:59 +0200

    Added checks in script

--- scripts/run_and_convert_notebook.sh
+++ scripts/run_and_convert_notebook.sh
@@ -5,8 +5,20 @@ NOTEBOOK_PATH="$1"
 ENV_NAME="$2"  # Second argument is the environment name
 OUTPUT_DIR="documentation/source/"
 
+# Check if the notebook path is empty
+if [ -z "$NOTEBOOK_PATH" ]; then
+    echo "No notebook path provided."
+    exit 0  # Exit successfully as there's nothing to do
+fi
+
+# Check if the notebook file exists
+if [ ! -f "$NOTEBOOK_PATH" ]; then
+    echo "Notebook path does not exist: $NOTEBOOK_PATH"
+    exit 1  # Exit with an error as the notebook path is invalid
+fi
+
 # Ensure the virtual environment is activated
-source "${ENV_NAME}/bin/activate"  # Updated to use the variable
+source "${ENV_NAME}/bin/activate"
 
 # Convert the notebook
 jupyter nbconvert --to markdown --execute --output-dir="$OUTPUT_DIR" "$NOTEBOOK_PATH"
