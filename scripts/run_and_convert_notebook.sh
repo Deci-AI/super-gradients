3defe66ae3ff130823313a659e1f28e53e9773f3
shayaharon <shay.aharon@deci.ai>
2024-01-22 14:52:15 +0200

    fixed env being passed to script

--- scripts/run_and_convert_notebook.sh
+++ scripts/run_and_convert_notebook.sh
@@ -2,10 +2,11 @@
 
 # Script to run and convert a single notebook
 NOTEBOOK_PATH="$1"
+ENV_NAME="$2"  # Second argument is the environment name
 OUTPUT_DIR="documentation/source/"
 
 # Ensure the virtual environment is activated
-source << parameters.sg_new_env_name >>/bin/activate
+source "${ENV_NAME}/bin/activate"  # Updated to use the variable
 
 # Convert the notebook
 jupyter nbconvert --to markdown --execute --output-dir="$OUTPUT_DIR" "$NOTEBOOK_PATH"
