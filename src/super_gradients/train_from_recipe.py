7d82773c23f0c228ab0d67e5126e79b88656ca3d
Adam Maciaszek <adam.maciaszek@pascal02.intra.deepsense.ai>
2024-01-30 15:00:33 +0100

    minor changes

--- src/super_gradients/train_from_recipe.py
+++ src/super_gradients/train_from_recipe.py
@@ -15,10 +15,12 @@ from super_gradients import Trainer, init_trainer
 
 load_dotenv()
 
+force_build = False
+gpu = modal.gpu.A100(count=1)
 stub = modal.Stub(name="train_from_recipe")    
 github_secret = modal.Secret.from_dotenv()
 
-image = Image.from_dockerfile("Dockerfile", force_build=True) \
+image = Image.from_dockerfile("Dockerfile", force_build=force_build) \
     .copy_local_dir("./src/super_gradients/recipes", "/root/recipes") \
     .pip_install_private_repos(
     "github.com/Unstructured-IO/super-gradients-fork.git@adam/modal-ai-tests",
@@ -28,10 +30,13 @@ image = Image.from_dockerfile("Dockerfile", force_build=True) \
 logger = getLogger(__name__)
 
 # @hydra.main(config_path="recipes", version_base="1.2")
-@stub.function(image=image)
+@stub.function(image=image, gpu=gpu)
 def _main() -> None:
     with hydra.initialize(config_path="recipes"):
         cfg = hydra.compose(config_name="cifar10_resnet")
+        cfg.training_hyperparams.max_epochs = 1
+        # cfg.multi_gpu = "DDP"
+        # cfg.num_gpus = 2
     logger.info(f"Config:\n{cfg}")
     Trainer.train_from_config(cfg)
 
@@ -39,6 +44,7 @@ def _main() -> None:
 def main() -> None:
     init_trainer()  # `init_trainer` needs to be called before `@hydra.main`
     _main.remote()
+    # _main.local()
 
 
 if __name__ == "__main__":
