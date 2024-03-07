f745b9f14cfc7bd73f3feec9678a871da3310964
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2024-03-07 22:55:49 +0200

    Explicitly specify a kernel name that match the environment we are in. This is to have correct "pip install" in the notebook

--- scripts/run_and_convert_notebook.sh
+++ scripts/run_and_convert_notebook.sh
@@ -24,7 +24,7 @@ source "${ENV_NAME}/bin/activate"
 
 # Convert the notebook
 # An --stdout flag is used to print the output to the console (Helpful for debugging if notebook conversion fails)
-jupyter nbconvert --to markdown --execute --output-dir="$OUTPUT_DIR" "$NOTEBOOK_PATH" --stdout
+jupyter nbconvert --to markdown --execute --output-dir="$OUTPUT_DIR" "$NOTEBOOK_PATH" --stdout --ExecutePreprocessor.kernel_name="$ENV_NAME"
 
 # Check for errors
 if [ $? -ne 0 ]; then
